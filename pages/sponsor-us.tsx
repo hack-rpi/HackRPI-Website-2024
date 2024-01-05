1bded2d46a00e9106a73d0638ea533f32a1aed67
Cooper Werner <cooperwerner824@gmail.com>
2024-01-04 19:29:46 -0500

    Setup Page Routing and Component Organization

---
+++ pages/sponsor-us.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function SponsorUs() {
+	return (
+		<div>
+			<Head>
+				<title>Sponsor Us</title>
+			</Head>
+			<div className="container">
+				<h1>Sponsor Us</h1>
+				<p>
+					We are looking for sponsors to help us cover the costs of running the conference. If you are interested in
+					sponsoring, please contact us at <a href="mailto:hackrpi@rpi.edu">hackrpi@rpi.edu</a>
+				</p>
+			</div>
+		</div>
+	);
+}
