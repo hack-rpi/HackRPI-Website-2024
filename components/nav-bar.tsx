67517a6aaac84f3ade02219359f18127f1a0e4b8
Cooper Werner <cooperwerner824@gmail.com>
2024-01-30 17:00:37 -0500

    Very Basic Nav Bar

---
+++ components/nav-bar.tsx
@@ -0,0 +1,13 @@
+import NextLink from "next/link";
+import NavBarLink from "./nav-bar-link";
+
+export default function NavBar() {
+	return (
+		<div className="bg-hackrpi-secondary-yellow h-16">
+			<div className="flex justify-start items-center h-full">
+				<NavBarLink href="/">HackRPI logo here</NavBarLink>
+                <NavBarLink href="/sponsor-us">Sponsor Us</NavBarLink>
+			</div>
+		</div>
+	);
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,11 @@
+import NavBar from "@/components/nav-bar";
 import "../app/globals.css";
 import Link from "../components/link";
 
 export default function Home() {
 	return (
 		<div>
-			<h1 className="font-sans">Home</h1>
-			<p>
-				Welcome to HackRPI 2024! We are still in the process of building this website, so please check back later for
-				more information.
-			</p>
-			<Link href="/sponsor-us">Sponsor Us</Link>
+			<NavBar></NavBar>
 		</div>
 	);
 }
