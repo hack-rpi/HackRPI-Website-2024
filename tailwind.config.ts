ad023e2c60ddf98d0af1783ceb0f944c79d016a2
Cooper Werner <cooperwerner824@gmail.com>
2024-01-30 16:19:05 -0500

    adding theme support and color variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,33 @@ const config: Config = {
 		"./app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"hackrpi-primary-blue": "#74b7ef",
+				"hackrpi-primary-dark-green": "#264e33",
+				"hackrpi-primary-light-green": "#88b63a",
+				"hackrpi-secondary-grey": "#efefef",
+				"hackrpi-secondary-light-blue": "#4a6277",
+				"hackrpi-secondary-dark-blue": "#27303b",
+				"hackrpi-secondary-dark-green": "#27303b",
+				"hackrpi-secondary-light-green": "#6d8740",
+				"hackrpi-secondary-yellow": "#edd559",
+			},
+		},
 	},
-	plugins: [],
+	plugins: [require("daisyui")],
+	daisyui: {
+		themes: [
+			{
+				hackrpi: {
+					primary: "#74b7ef",
+					secondary: "#264e33",
+					accent: "#edd559",
+					neutral: "#efefef",
+					"base-100": "#27303b",
+				},
+			},
+		],
+	},
 };
 export default config;
