4789ef3704524ab07211934cb2876a6e08e49672
Cooper Werner <cooperwerner824@gmail.com>
2024-01-30 22:09:38 -0500

    Title Components V1

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,15 @@ export default function Home() {
 	return (
 		<div className="">
 			<NavBar></NavBar>
-			<div className="absolute font-sans">
-				
+			<div className="absolute  top-[25%] left-0 w-[calc(100vw-450px)] lg:w-[calc(100vw-550px)] font-sans font-bold text-right flex items-end justify-center flex-col">
+				<h1 className="md:text-7xl lg:text-8xl xl:text-9xl">
+					HackRPI 11
+				</h1>
+				<h2 className="md:text-5xl lg:text-5xl xl:text-6xl">
+					Urban Upgrades
+				</h2>
+				<p className="md:text-4xl xl:text-5xl"> xx/xx/xx </p>
+				<p className="md:text-2xl lg:text-3xl"> Darrin Communications Center, <br /> Rensselaer Polytechnic Institute </p>
 			</div>
 			<SubwayMap></SubwayMap>
 		</div>
