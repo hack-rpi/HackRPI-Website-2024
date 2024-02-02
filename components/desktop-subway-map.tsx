70b0ef578d66af962b7296970a536566fe9e4828
Cooper Werner <cooperwerner824@gmail.com>
2024-02-02 16:36:37 -0500

    Subway Lines are Done for Desktop

--- components/desktop-subway-map.tsx
+++ components/desktop-subway-map.tsx
@@ -22,10 +22,10 @@ export default function DesktopSubwayMap() {
 			<div className="relative -top-[150%] right-[75%] w-[21rem] h-3 bg-hackrpi-secondary-yellow rotate-45 -z-[5]"></div>
 			{/* right Green */}
 			<div className="relative -top-[20%] -right-[75%] w-[45%] h-3 bg-hackrpi-primary-light-green rotate-45"></div>
-			<div className="relative top-[20%] left-[121%] w-3 h-[1000px] bg-hackrpi-primary-light-green "></div>
+			<div className="relative -top-[5%] left-[110%] w-3 h-[1000px] bg-hackrpi-primary-light-green "></div>
 			{/* bottom Blue */}
-			{/* <div className="relative top-[520px] right-[250px] w-[400px] h-3 bg-hackrpi-primary-blue -rotate-45"></div>
-				<div className="relative top-[660px] left-[0px] w-[calc(100vw-580px)] h-3 bg-hackrpi-primary-blue "></div> */}
+			<div className="relative bottom-[390%] right-[60%] w-[100%] h-3 bg-hackrpi-primary-blue -rotate-45"></div>
+			<div className="relative -top-[350%] right-[315%] w-[270%] h-3 bg-hackrpi-primary-blue "></div>
 			{/* Subway Stops */}
 			{/* <SubwayStop color="hackrpi-secondary-yellow" top="100px" left="75%"></SubwayStop> */}
 		</div>
