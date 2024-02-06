760614be9621774ada01af8fb401407da7e55454
Cooper Werner <cooperwerner824@gmail.com>
2024-02-06 16:24:12 -0500

    Add Logos to site

--- components/nav-bar/nav-bar.tsx
+++ components/nav-bar/nav-bar.tsx
@@ -3,9 +3,14 @@ import NavBarLink from "./nav-bar-link";
 
 export default function NavBar() {
 	return (
-		<div className="bg-hackrpi-secondary-yellow w-full h-12">
+		<div className="bg-hackrpi-secondary-yellow w-full h-16">
 			<div className="flex justify-start items-center h-full">
-				<NavBarLink href="/">HackRPI logo here</NavBarLink>
+				<NavBarLink href="/">
+					<div className="flex justify-around items-center">
+						<img src="HackRPI_Logo_Outlined.png" className="h-14" />
+						<h1 className="text-black text-2xl font-bold ml-2">HackRPI 11</h1>
+					</div>
+				</NavBarLink>
 				<NavBarLink href="/sponsor-us">Sponsor Us</NavBarLink>
 			</div>
 		</div>
