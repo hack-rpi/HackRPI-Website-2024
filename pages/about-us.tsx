e4975d37dd9a3d17573f013f84e653a1cd0ce613
sdhoat <121777913+sdhoat@users.noreply.github.com>
2024-02-06 17:47:03 -0500

    2/6/24

--- pages/about-us.tsx
+++ pages/about-us.tsx
@@ -8,45 +8,47 @@ const AboutPage = () => {
   };
 
   return (
-    <div id="about" className="text-center w-80 mx-auto">
-      <h1 className="font- text-5xl mt-8 mb-4">HackRPI 11</h1>
+    <div id="about" className="text-center w-90">
+      <h1 className="text-hackrpi-primary-blue text-5xl mt-8 mb-4">HackRPI 11</h1>
       <div className="p-4">
         <button
-          className={`button ${activeTab === 'one' ? '#74b7ef #264e33' : 'bg-red-700 text-white'}`}
+          className={`button ${activeTab === 'one' ? 'text-hackrpi-primary-blue' : 'text-hackrpi-secondary-grey'}`}
           onClick={() => handleTabClick('one')}
         >
           the event
         </button>
         <button
-          className={`button ${activeTab === 'two' ? 'bg-white text-black' : 'bg-red-700 text-white'}`}
+          className={`button ${activeTab === 'two' ? 'text-hackrpi-secondary-grey' : 'text-hackrpi-secondary-grey'}`}
           onClick={() => handleTabClick('two')}
         >
           the team
         </button>
       </div>
+	
 
-      <div className="text-left">
-        {activeTab === 'one' ? (
-          <p>
-            HackRPI 11 is the 11th annual intercollegiate hackathon hosted by RPI for students not only from RPI but also from all around the
-            world. Get swag and free food as you compete for exciting prizes! With a broad range of workshops and mentors on-site, there’s no
-            experience necessary to attend. Hackers will also have the opportunity to network with our fantastic sponsors from the
-            companies that make our event possible. This year's theme,{' '}
-            <span className="font-bold text-white">Urban Planning</span> is an ode to a decade inspiring students and HackRPI!
-            <br /><br />Come join us this coming TBD to hack, learn, and meet other people!
-          </p>
-        ) : (
-          <p>
-            We are a motivated team of RPI students who share a passion for exploring the bounds of computer science and a commitment to
-            organizing a fantastic event. Our team of students from every grade level works together to plan the hackathon and find sponsors
-            to bring the event together.
-            <br /><br />
-            We believe that HackRPI is a vital part of our school. Our goal is to inspire and challenge innovators, creators, developers, and
-            entrepreneurs in New York’s Tech Valley and beyond. We help attendees develop their skills, explore new technologies, and turn
-            their interests and visions into opportunities. Staying true to our slogan “Why Not Code the World?"
-          </p>
-        )}
+	  <div className="flex">
+      <div className="text-center w-1/3 p-4 border text-hackrpi-secondary-grey">
+        <p>
+			HackRPI X is the 10th annual intercollegiate hackathon hosted by RPI for students not only from RPI but also from all around the
+			world. Get swag and free food as you compete for exciting prizes! With a broad range of workshops and mentors on-site, there’s no
+			experience necessary to attend. Hackers will also have the opportunity to network with our fantastic sponsors from the
+			companies that make our event possible. This year's theme,{' '}
+			<span className="font-bold text-hackrpi-primary-blue">Urban Upgrade</span> is an ode to a decade inspiring students and HackRPI!
+			<br /><br />Come join us this coming TBD to hack, learn, and meet other people!
+        </p>
       </div>
+      <div className="w-1/3 p-4 border text-hackrpi-secondary-grey">
+        <p>
+			We are a motivated team of RPI students who share a passion for exploring the bounds of computer science and a commitment to
+			organizing a fantastic event. Our team of students from every grade level works together to plan the hackathon and find sponsors
+			to bring the event together.
+			<br /><br />
+			We believe that HackRPI is a vital part of our school. Our goal is to inspire and challenge innovators, creators, developers, and
+			entrepreneurs in New York’s Tech Valley and beyond. We help attendees develop their skills, explore new technologies, and turn
+			their interests and visions into opportunities. Staying true to our slogan “Why Not Code the World?"
+        </p>
+      </div>
+     </div>
 
       <div className="flex justify-center items-center mt-8">
         <div className="circle"></div>
@@ -67,7 +69,7 @@ const AboutPage = () => {
         <div>
           <a href="https://organize.mlh.io/participants/events/9892-hackrpi">
             <button
-              className="button bg-red-700 text-white"
+              className="button text-hackrpi-secondary-grey"
             >
               Register Now!
             </button>
