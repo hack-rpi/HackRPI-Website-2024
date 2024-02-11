451189c04223e25398ffdd3fb25f5f1302287f84
andrewtarnavsky <andrew.tarnavsky@gmail.com>
2024-02-10 19:22:47 -0500

    Added the hover effect to all images

--- components/sponsors.tsx
+++ components/sponsors.tsx
@@ -21,11 +21,11 @@ const SponsorPage = () => {
           ];
           return (
             <div className="container" key={'sponsorGroup' + indx}>
-            <h3 className="font-helvetica text-xl tierHeader mb-5 border-b-2 border-white pb-2 relative cursor-pointer group">
-            <span className="transition-all duration-250 group-hover:font-bold group-hover:text-light-blue-500">
-                {tierList[indx]}
-            </span>
-            </h3>
+              <h3 className="font-helvetica text-xl tierHeader mb-5 border-b-2 border-white pb-2 relative cursor-pointer group">
+                <span className="transition-all duration-250 group-hover:font-bold group-hover:text-light-blue-500">
+                  {tierList[indx]}
+                </span>
+              </h3>
               <div className="flex justify-center flex-wrap">
                 {tierList[indx] === 'Obsidian'
                   ? sponsorGroup
@@ -49,6 +49,7 @@ const SponsorPage = () => {
                             >
                               <div
                                 className="flex flex-col items-center justify-center innerSponsor m-10"
+                                style={{ cursor: 'pointer' }} // Add cursor pointer
                               >
                                 <img
                                   src={
@@ -58,6 +59,9 @@ const SponsorPage = () => {
                                   }
                                   className="img-fluid rounded w-32 h-auto"
                                   alt={sponsor.name}
+                                  style={{ transition: 'transform 0.3s ease' }} // Add transition
+                                  onMouseOver={(e) => e.currentTarget.style.transform = 'scale(1.1)'} // Apply hover effect
+                                  onMouseOut={(e) => e.currentTarget.style.transform = 'scale(1)'} // Remove hover effect
                                 />
                               </div>
                             </a>
@@ -82,6 +86,7 @@ const SponsorPage = () => {
                           >
                             <div
                               className="flex flex-col items-center justify-center innerSponsor m-10"
+                              style={{ cursor: 'pointer' }} // Add cursor pointer
                             >
                               <img
                                 src={
@@ -91,6 +96,9 @@ const SponsorPage = () => {
                                 }
                                 className="img-fluid rounded w-32 h-auto"
                                 alt={sponsor.name}
+                                style={{ transition: 'transform 0.3s ease' }} // Add transition
+                                onMouseOver={(e) => e.currentTarget.style.transform = 'scale(1.1)'} // Apply hover effect
+                                onMouseOut={(e) => e.currentTarget.style.transform = 'scale(1)'} // Remove hover effect
                               />
                             </div>
                           </a>
