b1e2b37777fb3bf8f504bc362e6faef71d214c43
Cooper Werner <cooperwerner824@gmail.com>
2024-02-28 21:12:59 -0500

    removing title because its going to change

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
+import Footer from "@/components/footer";
 import "../app/globals.css";
 import NavBar from "@/components/nav-bar/nav-bar";
 import TitleComponent from "@/components/title-components/title";
 
 export default function Home() {
 	return (
-		<div className="h-screen overflow-x-hidden overflow-y-visible">
+		<div className="h-screen overflow-x-hidden">
 			<NavBar />
-			<TitleComponent />
+			<Footer />
 		</div>
 	);
 }
