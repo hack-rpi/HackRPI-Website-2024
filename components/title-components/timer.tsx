cbe80dad6b181ff8aac5f49e424655a05b20485f
Cooper Werner <cooperwerner824@gmail.com>
2024-03-01 16:15:58 -0500

    timer design text updates

--- components/title-components/timer.tsx
+++ components/title-components/timer.tsx
@@ -3,19 +3,19 @@ export default function Timer() {
 		<div className="w-full h-fit flex flex-col items-center justify-center pl-12">
 			<div className="flex items-center justify-between w-full mb-4">
 				<Circle bgColor="bg-[#ef3a42]" textColor="text-white">
-					1
+					99
 				</Circle>
 				<Circle bgColor="bg-[#f8a13a]" textColor="text-white">
-					2
+					99
 				</Circle>
 				<Circle bgColor="bg-[#00a65c]" textColor="text-white">
-					3
+					99
 				</Circle>
 				<Circle bgColor="bg-[#0058a9]" textColor="text-white">
-					4
+					99
 				</Circle>
 				<Circle bgColor="bg-[#b43c96]" textColor="text-white">
-					5
+					99
 				</Circle>
 			</div>
 			<div className="flex items-center justify-between w-full">
@@ -42,7 +42,7 @@ export default function Timer() {
 function Circle({ bgColor, textColor, children }: { bgColor: string; textColor: string; children: React.ReactNode }) {
 	return (
 		<div
-			className={`w-1/6 aspect-square rounded-full flex items-center justify-center ${bgColor} ${textColor} text-6xl font-bold shadow-lg`}
+			className={`w-1/6 aspect-square rounded-full flex items-center justify-center ${bgColor} ${textColor} text-5xl lg:text-6xl xl:text-7xl font-bold shadow-lg`}
 		>
 			{children}
 		</div>
