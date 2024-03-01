2fb62e7a6f66bb3dfcf330a8343359571acdc6d1
Cooper Werner <cooperwerner824@gmail.com>
2024-03-01 11:58:10 -0500

    Timer Circles V1 Not Sure How I feel about this

--- components/title-components/timer.tsx
+++ components/title-components/timer.tsx
@@ -1,10 +1,50 @@
 export default function Timer() {
 	return (
-		<div>
-			{/* Text components */}
-			<div className="absolute top-[610px] left-0 flex items-center justify-center w-[calc(100vw-580px)]">
-				<h3 className="text-2xl xl:text-4xl">1 Month, 1 Day, 1 Hour, 1 Minute, 1 Second</h3>
+		<div className="w-full h-fit flex flex-col items-center justify-center pl-12">
+			<div className="flex items-center justify-between w-full mb-4">
+				<Circle bgColor="bg-hackrpi-primary-light-green" textColor="text-black">
+					1
+				</Circle>
+				<Circle bgColor="bg-hackrpi-secondary-yellow" textColor="text-black">
+					2
+				</Circle>
+				<Circle bgColor="bg-hackrpi-primary-dark-green" textColor="text-white">
+					3
+				</Circle>
+				<Circle bgColor="bg-hackrpi-primary-blue" textColor="text-black">
+					4
+				</Circle>
+				<Circle bgColor="bg-hackrpi-secondary-light-blue" textColor="text-white">
+					5
+				</Circle>
 			</div>
+			<div className="flex items-center justify-between w-full">
+				<Circle bgColor="bg-hackrpi-secondary-yellow" textColor="text-black">
+					M
+				</Circle>
+				<Circle bgColor="bg-hackrpi-primary-blue" textColor="text-black">
+					D
+				</Circle>
+				<Circle bgColor="bg-hackrpi-secondary-light-blue" textColor="text-white">
+					H
+				</Circle>
+				<Circle bgColor="bg-hackrpi-primary-dark-green" textColor="text-white">
+					M
+				</Circle>
+				<Circle bgColor="bg-hackrpi-primary-light-green" textColor="text-black">
+					S
+				</Circle>
+			</div>
+		</div>
+	);
+}
+
+function Circle({ bgColor, textColor, children }: { bgColor: string; textColor: string; children: React.ReactNode }) {
+	return (
+		<div
+			className={`w-1/6 aspect-square rounded-full flex items-center justify-center ${bgColor} ${textColor} text-6xl font-bold shadow-lg`}
+		>
+			{children}
 		</div>
 	);
 }
--- components/title-components/title-text.tsx
+++ components/title-components/title-text.tsx
@@ -16,9 +16,9 @@ export default function TitleText() {
 	}, []);
 
 	return (
-		<div className="w-fit font-sans  text-left flex items-start justify-center flex-col">
+		<div className="w-full font-sans  text-left flex items-start justify-center flex-col pl-12">
 			<h1 className="w-full text-8xl font-bold">HACKRPI</h1>
-			<h2 className="w-full text-[4rem] mb-8 h-20">Urban Upgrades</h2>
+			<h2 className="w-full text-[4rem] mb-8 h-20 whitespace-nowrap">Urban Upgrades</h2>
 			<p className="w-full text-4xl mb-8">
 				November 9-10, <span onMouseEnter={() => setYear(1824)}>{year}</span>
 			</p>
