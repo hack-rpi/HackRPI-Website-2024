573c823d814471685fe4693791c0a6042c187342
Cooper Werner <cooperwerner824@gmail.com>
2024-03-01 10:52:37 -0500

    Minor updates to the registration button

--- components/registration-button.tsx
+++ components/registration-button.tsx
@@ -1,7 +1,7 @@
 export default function RegistrationButton() {
 	return (
 		<div className="flex items-center justify-center">
-			<a href="https://mlh.io" className="btn btn-secondary rounded-full w-48">
+			<a href="https://mlh.io" className="btn btn-secondary rounded-full w-64 text-xl">
 				REGISTER NOW
 			</a>
 		</div>
