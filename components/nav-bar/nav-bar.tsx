3890552d0d726ea83c8a4af9b6afe4d7b7ef035b
Cooper Werner <cooperwerner824@gmail.com>
2024-03-03 18:03:46 -0500

    Mobile Optimized Nav Bar

--- components/nav-bar/nav-bar.tsx
+++ components/nav-bar/nav-bar.tsx
@@ -3,24 +3,43 @@ import DesktopNavBar from "./nav-bar-desktop";
 import MobileNavBar from "./nav-bar-mobile";
 import { NavBarLinkType } from "@/types/nav-bar-links";
 
-export default function NavBar() {
+export default function NavBar({ showOnScroll }: { showOnScroll: boolean }) {
 	const links: NavBarLinkType[] = [
 		{ href: "/", children: "Home" },
 		{ href: "/sponsor-us", children: "Sponsor Us" },
 	];
 
+	const [showNav, setShowNav] = useState(false);
 	const [windowWidth, setWindowWidth] = useState(0);
 
+	// Sets the scroll threshold to the viewport height
+	var scrollThreshold = 1080;
+	const navHeight = 96;
+
+	// Add event listener to the window to update the scrollY state
 	useEffect(() => {
+		scrollThreshold = window.innerHeight - navHeight;
 		setWindowWidth(window.innerWidth);
+		const handleScroll = () => {
+			setShowNav(window.scrollY > scrollThreshold);
+		};
 		const handleResize = () => {
 			setWindowWidth(window.innerWidth);
 		};
+		window.addEventListener("scroll", handleScroll);
 		window.addEventListener("resize", handleResize);
+
 		return () => {
+			window.removeEventListener("scroll", handleScroll);
 			window.removeEventListener("resize", handleResize);
 		};
 	}, []);
 
-	return windowWidth > 860 ? <DesktopNavBar links={links} /> : <MobileNavBar links={links} />;
+	if (windowWidth < 860) return <MobileNavBar links={links} />;
+
+	return (
+		<div className={`${showOnScroll ? (showNav ? "top-0" : "-top-24") : "top-0"} fixed transition-all w-full z-10`}>
+			<DesktopNavBar links={links} />
+		</div>
+	);
 }
