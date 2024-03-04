cebc345afe00ecf5948e543bef730b22cb49ac1c
Cooper Werner <cooperwerner824@gmail.com>
2024-03-03 20:38:48 -0500

    Animated Interactive Subway Stops

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,27 @@ import TitleComponent from "@/components/title-components/title";
 import FAQPage from "@/components/faq/faq";
 import NavBar from "@/components/nav-bar/nav-bar";
 import MlhBanner from "@/components/mlh-banner/mlh-banner";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+	let faqStart = 0;
+	let faqEnd = 0;
+	const [highlightFAQ, setHighlightFAQ] = useState(false);
+
+	useEffect(() => {
+		faqStart = (document.getElementById("faq")?.offsetTop || window.innerHeight) - 140;
+		faqEnd = faqStart + (document.getElementById("faq")?.offsetHeight || window.innerHeight);
+		console.log(faqStart, faqEnd);
+		const handleScroll = () => {
+			setHighlightFAQ(window.scrollY > faqStart && window.scrollY < faqEnd);
+			console.log(window.scrollY > faqStart && window.scrollY < faqEnd);
+		};
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
 	return (
 		<div className="flex flex-col items-start  desktop:items-center justify-start w-full">
 			<MlhBanner />
@@ -12,6 +31,18 @@ export default function Home() {
 				<TitleComponent />
 				<FAQPage />
 			</div>
+
+			{/* Subway Motif */}
+			{/* This code is kinda jank but it works */}
+			<div className="absolute w-3 top-[500px] desktop:top-[100vh] right-8 h-[1300px] desktop:h-[1000px] bg-hackrpi-primary-blue"></div>
+			<div
+				className={`${
+					highlightFAQ
+						? "fixed top-32 bg-opacity-100"
+						: "absolute top-[532px] desktop:top-[calc(100vh+64px)] bg-opacity-0"
+				} w-12 h-12 rounded-full bg-white border-[6px] border-hackrpi-primary-blue right-[.9rem]
+				transition-colors duration-300`}
+			></div>
 		</div>
 	);
 }
