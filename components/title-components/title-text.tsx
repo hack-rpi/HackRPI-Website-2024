d9ede8c63351c014c55a0d958e289c77fd4d74c0
Cooper Werner <cooperwerner824@gmail.com>
2024-03-04 11:03:57 -0500

    improving the count up

--- components/title-components/title-text.tsx
+++ components/title-components/title-text.tsx
@@ -7,13 +7,24 @@ export default function TitleText() {
 	// This kinda works, we may want to add a delay before this starts counting
 	// We may also want to make the count ease in and out
 	useEffect(() => {
-		const interval = setInterval(() => {
-			setYear((prev) => {
-				return prev + 1 > 2024 ? 2024 : prev + 1;
-			});
-		}, 10);
-		return () => clearInterval(interval);
-	}, []);
+		const startCounting = setTimeout(() => {
+			const target = 2024;
+
+			const interval = setInterval(() => {
+				setYear((prev) => {
+					if (prev < target) return prev + 1;
+					else {
+						clearInterval(interval);
+						return prev;
+					}
+				});
+			}, 10);
+
+			return () => clearInterval(interval);
+		}, 700);
+
+		return () => clearTimeout(startCounting);
+	}, [year]);
 
 	return (
 		<div className="w-full font-sans  text-left flex items-start justify-center flex-col mb-8">
