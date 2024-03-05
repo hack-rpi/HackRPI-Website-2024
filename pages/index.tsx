966a5b88693aa08390c44fdf99de2100408426a1
Cooper Werner <cooperwerner824@gmail.com>
2024-03-04 21:35:56 -0500

    fix build

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Footer from "@/components/footer";
 import "../app/globals.css";
 import NavBar from "@/components/nav-bar/nav-bar";
-import TitleComponent from "@/components/title-components/title";
 import FAQPage from "@/components/faq/faq";
 
 export default function Home() {
