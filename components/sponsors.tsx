b0fe729d8a4a642cae0c02860c05de0a5441fbbe
Cooper Werner <cooperwerner824@gmail.com>
2024-03-11 20:58:07 -0400

    Integrating the Sponsors Component in a more seamless way.

--- components/sponsors.tsx
+++ components/sponsors.tsx
@@ -3,13 +3,13 @@ import sponsorsJson from "../../HackRPI-Website-2024/public/sponsors/sponsors.js
 import { SponsorsJSON, sponsorTiers } from "@/types/sponsorsType";
 import NextImage from "next/image";
 
-const SponsorPage = () => {
+const Sponsors = () => {
 	const tierList: sponsorTiers[] = ["Obsidian", "Gold", "Silver", "Bronze", "Collaborators"];
 	const [sponsors] = useState<SponsorsJSON>(sponsorsJson);
 
 	return (
-		<div className="flex flex-col w-full justify-start items-center bg-gradient-to-b from-hackrpi-secondary-dark-blue via-hackrpi-primary-blue to-hackrpi-secondary-dark-blue">
-			<div className="w-3/4">
+		<div className="flex flex-col w-full justify-center items-start desktop:items-center pl-8 bg-gradient-to-b from-hackrpi-secondary-dark-blue via-hackrpi-primary-blue to-hackrpi-secondary-dark-blue ">
+			<div className="w-5/6 desktop:w-11/12">
 				<h2 className="pb-10 text-white font-sans font-bold text-center text-4xl">
 					Thank you to our sponsors that make HackRPI possible!
 				</h2>
@@ -24,7 +24,7 @@ const SponsorPage = () => {
 									return (
 										<div
 											key={sponsor.name}
-											className="w-fit h-fit p-4 m-10 hover:scale-110 bg-black bg-opacity-0 hover:bg-opacity-15 rounded-md transition-all flex items-center justify-center mx-4"
+											className="w-fit h-fit p-4 m-4 hover:scale-110 bg-black bg-opacity-0 hover:bg-opacity-15 rounded-md transition-all flex items-center justify-center mx-4"
 										>
 											<a href={sponsor.url} target="_blank" rel="noreferrer">
 												<NextImage
@@ -46,4 +46,4 @@ const SponsorPage = () => {
 	);
 };
 
-export default SponsorPage;
+export default Sponsors;
