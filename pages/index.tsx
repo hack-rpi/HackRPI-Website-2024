b8be836d4c48c793feea771204ee7588b31350f2
sdhoat <121777913+sdhoat@users.noreply.github.com>
2024-03-16 22:15:10 -0400

    Adding New Link System for Maps

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,17 +5,38 @@ import MlhBanner from "@/components/mlh-banner/mlh-banner";
 import Head from "next/head";
 import TitleComponent from "@/components/title-components/title";
 import "../app/globals.css";
-import Link from "../components/link";
+import AboutSection from './about-us';
 
 export default function Home() {
 	return (
-		<div>
-			<h1 className="font-sans">Home</h1>
-			<p>
-				Welcome to HackRPI 2024! We are still in the process of building this website, so please check back later for
-				more information.
-			</p>
-			<Link href="/sponsor-us">Sponsor Us</Link>
-		</div>
+		<>
+			<Head>
+				<title>HackRPI 2024</title>
+				<meta
+					name="description"
+					content="HackRPI is RPI's annual intercollegiate hackathon hosted by students for students. Get swag and free food as you compete for exciting prizes! With a broad range of workshops and mentors on-site, there’s no experience necessary to attend."
+				/>
+			</Head>
+			<div className="flex flex-col items-start desktop:items-center justify-start">
+				<MlhBanner />
+				<NavBar showOnScroll={true} />
+				<div className="w-5/6 desktop:w-full pl-8 desktop:px-8">
+					<TitleComponent />
+					<FAQPage />
+				</div>
+				<div>
+      				<AboutSection />
+    			</div>
+				<Footer />
+
+				<div className="absolute w-3 top-[500px] desktop:top-[100vh] right-6 2xs:right-8 h-[1500px] desktop:h-[1000px] bg-hackrpi-primary-blue"></div>
+				<div
+					className={`absolute bg-hackrpi-secondary-dark-blue top-[460px] desktop:top-[100vh] w-12 h-12 rounded-full  border-[6px] border-hackrpi-primary-blue transition-colors duration-300 z-0 right-1.5 2xs:right-3.5`}
+				></div>
+				<div
+					className={`absolute bg-hackrpi-secondary-dark-blue top-[1960px] w-12 h-12 rounded-full  border-[6px] border-hackrpi-primary-blue transition-colors duration-300 z-0 right-1.5 2xs:right-3.5`}
+				></div>
+			</div>
+		</>
 	);
 }
