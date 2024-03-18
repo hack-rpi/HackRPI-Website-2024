6c07ca7cdab55b340d8eb3aa24b57a91e2956ad7
sdhoat <121777913+sdhoat@users.noreply.github.com>
2024-03-18 17:10:40 -0400

    Maps with bar and head, but missing some compenents

--- pages/maps.tsx
+++ pages/maps.tsx
@@ -1,21 +1,39 @@
 import React, { useState } from 'react';
 import DCC from './maps_img/DCC.png';
 import LOW from './maps_img/LOW.png';
+import NavBar from "@/components/nav-bar/nav-bar";
+import Head from "next/head";
+import Footer from "@/components/footer";
 
 const MapsPage = () => {
-  const [showDCC, setShowDCC] = useState(false);
+  const [showDCC, setShowDCC] = useState(true);
   const [showLOW, setShowLOW] = useState(false);
 
+  const handleShowDCC = () => {
+    setShowDCC(true);
+    setShowLOW(false);
+  };
+
+  const handleShowLOW = () => {
+    setShowDCC(false);
+    setShowLOW(true);
+  };
+
   return (
-    <div id="about" className="MapsPage text-center w-80 mx-auto">
-      <h1 className="title font-Mokoto text-5xl py-8">MAPS</h1>
-      
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        <div className="px-4">
-          <h2 className="text-xl font-semibold mb-4">Darrin Communications Center</h2>
-          <button onClick={() => setShowDCC(true)}>
-            Show DCC Map
-          </button>
+    <div>
+      <Head>
+        <title>Maps Page</title>
+      </Head>
+      <NavBar showOnScroll={true} />
+      <div id="about" className="MapsPage text-center mx-auto w-2/3">
+        <h1 className="title font-Mokoto text-5xl py-8">MAPS</h1>
+
+        <div className="flex flex-col items-center space-y-8">
+          <div className="w-full">
+            <button onClick={handleShowDCC} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-8 rounded-full mb-4">
+              Darrin Communications Center
+            </button>
+          </div>
           {showDCC && (
             <img
               src={DCC.src}
@@ -24,12 +42,12 @@ const MapsPage = () => {
               style={{ maxWidth: '100%' }}
             />
           )}
-        </div>
-        <div className="px-4">
-          <h2 className="text-xl font-semibold mb-4">Low Center for Industrial Innovation</h2>
-          <button onClick={() => setShowLOW(true)}>
-            Show LOW Map
-          </button>
+
+          <div className="w-full">
+            <button onClick={handleShowLOW} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-8 rounded-full mb-4">
+              Low Center for Industrial Innovation
+            </button>
+          </div>
           {showLOW && (
             <img
               src={LOW.src}
@@ -40,6 +58,7 @@ const MapsPage = () => {
           )}
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
