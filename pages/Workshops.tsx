adcf2eba20e47b730a9c50402d2cf1524bc1b1c1
andrewtarnavsky <andrew.tarnavsky@gmail.com>
2024-03-19 00:47:31 -0400

    Mapped out workshops
    Mapped out all workshops and added a card and modal styling as well as a hover effect

--- pages/Workshops.tsx
+++ pages/Workshops.tsx
@@ -1,18 +1,26 @@
 import React, { useState } from 'react';
-import workshopMern from '../../public/workshopPics/mern.png';
-import workshopPassword from '../../public/workshopPics/password.jpeg';
-import workshopHacking from '../../public/workshopPics/hacking.jpeg';
-import workshopSafety from '../../public/workshopPics/safety.jpeg';
-import workshopDesign from '../../public/workshopPics/design.jpeg';
-import workshopAI from '../../public/workshopPics/ai.jpeg';
-import workshopAgile from '../../public/workshopPics/agile.jpeg';
-import workshopForge from '../../public/workshopPics/forge.png';
-import workshopMobdev from '../../public/workshopPics/mobdev.png';
-import workshopQuantum from '../../public/workshopPics/quantum.png';
-import workshopSemi from '../../public/workshopPics/semi.jpeg';
-import workshopTechstack from '../../public/workshopPics/techstack.jpeg';
+import "../app/globals.css";
+import workshopMern from './../public/workshopPics/mern.png';
+import workshopPassword from './../public/workshopPics/password.jpeg';
+import workshopHacking from './../public/workshopPics/hacking.jpeg';
+import workshopSafety from './../public/workshopPics/safety.jpeg';
+import workshopDesign from './../public/workshopPics/design.jpeg';
+import workshopAI from './../public/workshopPics/ai.jpeg';
+import workshopAgile from './../public/workshopPics/agile.jpeg';
+import workshopForge from './../public/workshopPics/forge.png';
+import workshopMobdev from './../public/workshopPics/mobdev.png';
+import workshopQuantum from './../public/workshopPics/quantum.png';
+import workshopSemi from './../public/workshopPics/semi.jpeg';
+import workshopTechstack from './../public/workshopPics/techstack.jpeg';
 
-
+interface Workshop {
+    title: string;
+    time: string;
+    location: string;
+    speaker: string;
+    pic: string;
+    description: string;
+}
 
     const workshops = [
         {
@@ -20,7 +28,7 @@ import workshopTechstack from '../../public/workshopPics/techstack.jpeg';
             time: '12-12:30pm',
             location: 'DCC 318',
             speaker: 'Kathy Rauch',
-            pic: workshopSafety,
+            pic: workshopSafety.src,
             description: 'This workshop is designed for hackers interested in the Patient Safety Tech Prize. It will\
             give you background on patient safety as an urgent issue and will help inspire you to\
             spend your weekend hacking together a project that has the potential to save lives! \
@@ -32,7 +40,7 @@ import workshopTechstack from '../../public/workshopPics/techstack.jpeg';
           time: '12:30-1:30pm',
           location: 'DCC 308',
           speaker: 'Michael Anderson and Konstantin Kuzmin',
-          pic: workshopTechstack,
+          pic: workshopTechstack.src,
           description: 'TBD'
         },
         
@@ -41,7 +49,7 @@ import workshopTechstack from '../../public/workshopPics/techstack.jpeg';
             time: '1:30-2:30pm',
             location: 'DCC 308',
             speaker: 'Yash Kaul',
-            pic: workshopMern,
+            pic: workshopMern.src,
             description: 'Gain hands-on experience with MongoDB—covering schema validation, client to database connection, and implementing API \
             controllers. Learn about Express: including routing, middleware, and error handling. Utilize React to quickly build \
             interactive applications with state management. Explore Node.js and Npm to manage package dependencies\
@@ -54,7 +62,7 @@ import workshopTechstack from '../../public/workshopPics/techstack.jpeg';
           time: '1:30-2:30pm',
           location: 'DCC 318',
           speaker: 'Ryan Hickey',
-          pic: workshopMobdev,
+          pic: workshopMobdev.src,
           description: "If you've ever wondered how those awesome apps on your phone are made, or if you've got an itch to create something fun and \
           innovative, the Mobile Dev Workshop is the perfect place to start! The main course of our workshop revolves around React and Flutter.  \
           These two powerful frameworks empower you to create fantastic, user-friendly, and visually stunning apps for both IOS and Android at  \
@@ -72,7 +80,7 @@ import workshopTechstack from '../../public/workshopPics/techstack.jpeg';
           time: '2:30-3:30pm',
           location: 'DCC 308',
           speaker: 'Brian Ingmanson',
-          pic: workshopQuantum,
+          pic: workshopQuantum.src,
           description: 'How folks can run their first quantum circuit on a real quantum computer, and some of the quirks of quantum computing.'
         },
 
@@ -81,7 +89,7 @@ import workshopTechstack from '../../public/workshopPics/techstack.jpeg';
           time: '3:30-4:30pm',
           location: 'DCC 324',
           speaker: 'John Sturman',
-          pic: workshopAgile,
+          pic: workshopAgile.src,
           description: 'Join John Sturman for a presentation about agile development and how to work in a development group for maximum productivity.\
           We will explore Scrum and iterative development doing a fun simulation exercise to give you some hands-on experience. \
           This is a good workshop to support you in working in a group creating a project efficiently and effectively.'
@@ -92,7 +100,7 @@ import workshopTechstack from '../../public/workshopPics/techstack.jpeg';
         time: '4:30-5:30pm',
         location: 'DCC 318',
         speaker: ' Dr. Brian Callahan (w/ Ayah Tharwat )',
-        pic: workshopHacking,
+        pic: workshopHacking.src,
         description: 'Securing software requires developers to learn how to think like an attacker; you cannot defend against what you cannot\
         imagine going  wrong with your software. There are a wide variety of potential attacks, but \
         learning just a basic few can go a very long way towards improving your coding skills. In this workshop, we will participate in a \
@@ -107,7 +115,7 @@ import workshopTechstack from '../../public/workshopPics/techstack.jpeg';
       time: '4:30-5:30pm',
       location: 'DCC 308',
       speaker: 'Kylinn Askew',
-      pic: workshopDesign,
+      pic: workshopDesign.src,
       description: 'Explore the power of design thinking to drive innovation and foster collaboration to build\
       strong technical products and startups. Learn how to harness creative problem-solving,\
       empathetic user-centric approaches, and cross-functional teamwork to create products\
@@ -119,7 +127,7 @@ import workshopTechstack from '../../public/workshopPics/techstack.jpeg';
         time: '5:30-6:30pm',
         location: 'DCC 318',
         speaker: 'Alex Vargas and Ryan Gallagher',
-        pic: workshopPassword,
+        pic: workshopPassword.src,
         description: 'Join the FBI as they explain the math behind password complexity, including tactics for how to attack and\
         break passwords. They will cover steps on how to extract a password hash from a file and run it with \
         hashcat to crack the password. '
@@ -130,7 +138,7 @@ import workshopTechstack from '../../public/workshopPics/techstack.jpeg';
       time: '5:30-6:30pm',
       location: 'DCC 324',
       speaker: 'Alexander Martin',
-      pic: workshopSemi,
+      pic: workshopSemi.src,
       description: 'Software needs hardware to operate. The last several years has seen a fundamental shift in the\
       Semiconductor Supply Chain. We will provide an overview of the Semiconductor Industry and the role of Global \
       Foundries in that ecosystem.'
@@ -141,7 +149,7 @@ import workshopTechstack from '../../public/workshopPics/techstack.jpeg';
         time: '6:30-7:30pm',
         location: 'DCC 324',
         speaker: 'Raven Levitt',
-        pic: workshopAI,
+        pic: workshopAI.src,
         description: 'Have a hack that needs AI but no idea where to start? In this workshop I’ll go over how to use the\
         Hugging Face API to easily leverage thousands of different machine learning models to tackle whatever\
           challenge you’re dealing with, be it text-generation, object detection, or just about any other machine \
@@ -154,7 +162,7 @@ import workshopTechstack from '../../public/workshopPics/techstack.jpeg';
         time: '7:30-8:30',
         location: 'Forge',
         speaker: 'Mikiel Gica, Saad Mujahid, and Julia Kapchynsky',
-        pic: workshopForge,
+        pic: workshopForge.src,
         description: 'The Forge will be open to anyone interested in designing and creating their\
         own contour-cut vinyl stickers. We will be holding a workshop on using free\
         design software to prepare designs for our Roland BN-20A, and \
@@ -162,6 +170,66 @@ import workshopTechstack from '../../public/workshopPics/techstack.jpeg';
         and a few images you want to turn into stickers is strongly recommended \
        to be able to follow along and participate.'
     },
-    ];
 
-    
+];
+
+
+const WorkshopPage = () => {
+    const [show, setShow] = useState(false);
+    const [selectedWorkshop, setSelectedWorkshop] = useState<Workshop | null>(null);
+
+    const handleShow = (workshop: Workshop) => {
+        setSelectedWorkshop(workshop);
+        setShow(true);
+    };
+
+    const handleClose = () => {
+        setSelectedWorkshop(null);
+        setShow(false);
+    };
+
+    return (
+        <div id="workshops" className="WorkshopPage text-center max-w-screen-xlg mx-auto px-6">
+            <h1 className="font-Mokoto text-6xl mb-10">Checkout our Workshops!</h1>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-16">
+                {workshops.map((workshop, index) => (
+                    <div key={index} className="mb-8">
+                        <div className="cursor-pointer" onClick={() => handleShow(workshop)}>
+                            <div className="group">
+                                <div className="workshop-container rounded-lg overflow-hidden shadow-lg transform transition-transform duration-300 hover:scale-105">
+                                    <img
+                                        src={workshop.pic}
+                                        className="rounded-t-lg h-48 object-cover mx-auto"
+                                        alt="workshop"
+                                    />
+                                    <div className="bg-transparent p-4">
+                                        <h2 className="text-white">{workshop.title}</h2>
+                                        <p className="text-sm"><span className="text-red font-bold">Time:</span> {workshop.time}</p>
+                                        <p className="text-sm"><span className="text-red font-bold">Location:</span> {workshop.location}</p>
+                                        <p className="text-sm"><span className="text-red font-bold">Speaker:</span> {workshop.speaker}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            {selectedWorkshop && (
+                <div>
+                    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
+                        <div className="bg-white p-8 rounded-lg">
+                            <h2 className="text-2xl font-bold mb-4">{selectedWorkshop.title}</h2>
+                            <p><strong className="text-red font-bold">Time:</strong> {selectedWorkshop.time}</p>
+                            <p><strong className="text-red font-bold">Location:</strong> {selectedWorkshop.location}</p>
+                            <p><strong className="text-red font-bold">Speaker:</strong> {selectedWorkshop.speaker}</p>
+                            <p>{selectedWorkshop.description}</p>
+                            <button className="bg-red text-white px-4 py-2 mt-4" onClick={handleClose}>Close</button>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default WorkshopPage;
