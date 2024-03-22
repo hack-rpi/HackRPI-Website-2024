ca48f1ea97c90347a94466ba79a0a5247d748a31
Cooper Werner <cooperwerner824@gmail.com>
2024-03-22 10:48:46 -0400

    minor update to logo sizing

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
 	return (
 		<div className="flex flex-col items-center justify-center w-full min-h-fit  z-10 bg-hackrpi-secondary-light-blue">
 			<div className="flex flex-col md:flex-row items-start md:items-center justify-center w-11/12 h-fit md:h-60 my-10">
-				<div className="w-1/6 h-full mr-4">
+				<div className="w-1/3 md:w-1/6 h-full mr-4">
 					<NextImage src={logo} alt="HackRPI Logo" className="h-full object-contain" />
 				</div>
 				<div className="w-full md:w-3/6 h-full mr-1 my-4">
