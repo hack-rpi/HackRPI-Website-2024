b7ece3ba3966b609e5ae80e659652ef3294f9594
Cooper Werner <cooperwerner824@gmail.com>
2024-04-01 20:39:32 -0400

    footer updates from marketing

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -6,11 +6,11 @@ import SocialLinks from "../socials-links/social-links";
 export default function Footer() {
 	return (
 		<div className="flex flex-col items-center justify-center w-full min-h-fit  z-10 bg-hackrpi-secondary-light-blue">
-			<div className="flex flex-col md:flex-row items-start md:items-center justify-center w-11/12 h-fit md:h-60 my-10">
-				<div className="w-1/3 md:w-1/6 h-full mr-4">
+			<div className="flex flex-col md:flex-row items-start md:items-center justify-center w-11/12 h-fit md:h-40 my-1">
+				<div className="w-1/4 md:w-1/6 h-full mr-4">
 					<NextImage src={logo} alt="HackRPI Logo" className="h-full object-contain" />
 				</div>
-				<div className="w-full md:w-3/6 h-full mr-1 my-4">
+				<div className="w-full md:w-2/6 h-full mr-1 my-4">
 					<div className=" w-11/12 flex flex-col justify-around items-start h-full">
 						<div className="mb-4">
 							<h2 className="text-2xl tracking-wider font-sans font-thin">
@@ -19,14 +19,14 @@ export default function Footer() {
 							<p>Darrin Communications Center @ Rensselaer Polytechnic Institute</p>
 							<p>110 8th St, Troy, NY 12180</p>
 						</div>
-						<p>
+						{/* <p>
 							HackRPI invites you to tackle the challenges of tomorrow&apos;s cities! Whether you&apos;re passionate
 							about sustainability, technology, or community development, come build the next big urban upgrade with us.
 							Secure your spot today!
-						</p>
+						</p> */}
 					</div>
 				</div>
-				<div className="w-3/4 md:w-1/4 border-l-2 border-l-white h-full">
+				<div className="w-3/4 md:w-1/3 md:border-l-2 border-l-white h-full">
 					<div className="w-full ml-2 h-full flex flex-col items-start justify-around">
 						<HackRPILink className="w-full mb-4" href="https://hackrpi.com">
 							Register Today and Join Us!
