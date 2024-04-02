cd88aa13b35c122de4c439af960bc4c2f7356990
Cooper Werner <cooperwerner824@gmail.com>
2024-04-01 20:52:19 -0400

    minor updates to the navbar links

--- components/nav-bar/nav-bar-desktop.tsx
+++ components/nav-bar/nav-bar-desktop.tsx
@@ -9,9 +9,7 @@ export default function DesktopNavBar({ links }: { links: NavBarLinkType[] }) {
 		<div className="bg-hackrpi-secondary-dark-blue w-full h-24">
 			<div className="flex justify-start items-center h-full">
 				<div className="w-[12.5%] flex items-center justify-center">
-					<NavBarLink href="/">
-						<NextImg alt="HackRPI Logo" aria-label="Home Page" src={logo} className="w-14 image-full" />
-					</NavBarLink>
+					<NextImg alt="HackRPI Logo" aria-label="Home Page" src={logo} className="w-14 image-full" />
 				</div>
 				<div className="w-1/2">
 					{links.map((link) => (
--- components/nav-bar/nav-bar-link.tsx
+++ components/nav-bar/nav-bar-link.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function NavBarLink({ href, children }: { href: string; children: React.ReactNode }) {
 	return (
 		<NextLink
-			className="w-fit px-1 mx-1 bg-opacity-0 bg-black hover:bg-opacity-15 text-white text-2xl transition-all rounded-md"
+			className="w-fit px-1 mx-1 text-white bg-gradient-to-r from-hackrpi-primary-blue to-hackrpi-secondary-light-green bg-[length:0%_3px] bg-no-repeat bg-left-bottom p-1.5 hover:bg-[length:100%_3px] transition-all duration-300 text-xl"
 			href={href}
 		>
 			{children}
