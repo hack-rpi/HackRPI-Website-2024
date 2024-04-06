165623bdd9eabd0bddbe6fc727d4437649f61243
Jordan <79342877+Jordan231111@users.noreply.github.com>
2024-04-05 22:22:24 -0400

    Made finalizing touches and best practices such as removing globals.css custom button and using tailwind css arbitrary

--- pages/sponsor-us-page.tsx
+++ pages/sponsor-us-page.tsx
@@ -20,18 +20,18 @@ function SponsorUsPage() {
 				></iframe>
 			</div>
 
-			<div className="text-left w-full">
-				<h3 className="text-2xl font-bold mb-4">This year we need your help...</h3>
-			</div>
-			<Link
-				href="https://tinyurl.com/hackrpi-donation"
-				className="btn btn-primary rounded-full bg-hackrpi-primary-blue border-none hover:bg-hackrpi-primary-blue-dark px-10 custom-btn-height"
-			>
-				Donate
-			</Link>
+<div className="text-left w-full pt-20">
+  <h3 className="text-4xl font-bold mb-4">This year we need your help...</h3>
+</div>
+<Link
+  href="https://tinyurl.com/hackrpi-donation"
+  className="btn btn-primary rounded-full bg-hackrpi-primary-blue border-none hover:bg-hackrpi-primary-blue-dark px-6 h-[50px] text-white text-[1.575rem]"
+>
+  Donate Now!
+</Link>
 			<div className="flex flex-col sm:flex-row justify-center w-full">
 				<div className="flex flex-col items-center w-full sm:w-1/2 p-2 sm:mr-4 mb-4 sm:mb-0">
-					<h1 className="text-2xl font-bold mb-4">Want to support?</h1>
+					<h1 className="text-2xl font-bold mb-4">Individual Donations</h1>
 					<iframe
 						className="w-full aspect-[8.5/11]"
 						src="https://drive.google.com/file/d/1n0A94WUZPzI5g1w46pR1VZ4bSXKOiFsy/preview"
@@ -40,7 +40,7 @@ function SponsorUsPage() {
 					></iframe>
 				</div>
 				<div className="flex flex-col items-center w-full sm:w-1/2 p-2 sm:ml-4">
-					<h3 className="text-2xl font-bold mb-4">Interested in sponsoring?</h3>
+					<h3 className="text-2xl font-bold mb-4">Company Sponsorships</h3>
 					<iframe
 						className="w-full aspect-[8.5/11]"
 						src="https://drive.google.com/file/d/17GguOFdq5aU5NuFXnHyLxxsXSzxryHQc/preview"
