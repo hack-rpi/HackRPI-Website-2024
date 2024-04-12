7ca6942d5d66550f8737cf6666844e4aafa7fcb3
Jordan <79342877+Jordan231111@users.noreply.github.com>
2024-04-12 00:00:02 -0400

    Implemented the carousel but still need to debug the issue with the images not showing up as well as image in wrong position

--- components/prev-projects/projectdisplay.tsx
+++ components/prev-projects/projectdisplay.tsx
@@ -1,30 +1,49 @@
-// components/ProjectDisplay.tsx
 import React from 'react';
+import Image from 'next/image';
 
-interface ProjectProps {
+export interface ProjectDisplayProps {
   title: string;
   authors: string[];
   description: string;
-  imageUrl: string;
+  imageUrl?: string;
+  imageOnLeft?: boolean;
+  textRightAlign?: boolean;
 }
 
-const ProjectDisplay: React.FC<ProjectProps> = ({ title, authors, description, imageUrl }) => {
-  return (
-    <div className="bg-white shadow-lg rounded-lg p-6 mb-8">
-      <h3 className="text-xl font-bold mb-2">{title}</h3>
-      <div className="mb-4">
-        <strong>By:</strong> {authors.join(', ')}
+const ProjectDisplay: React.FC<ProjectDisplayProps> = ({ title, authors, description, imageUrl, imageOnLeft, textRightAlign }) => {
+    return (
+      <div className="text-white p-6 rounded-lg mb-8 flex flex-col md:flex-row">
+        {imageOnLeft && imageUrl && (
+          <div className="flex-initial mr-4">
+            <Image
+              src={imageUrl}
+              alt={`Image for ${title}`}
+              width={400}
+              height={400}
+              className="object-cover rounded-lg"
+            />
+          </div>
+        )}
+        <div className={`flex-1 ${textRightAlign ? 'text-right' : ''}`}>
+          <h3 className="text-xl font-bold mb-2">{title}</h3>
+          <div className="mb-4">
+            <strong>By:</strong> {authors.join(', ')}
+          </div>
+          <p className="text-gray-300">{description}</p>
+        </div>
+        {!imageOnLeft && imageUrl && (
+          <div className="flex-initial ml-4">
+            <Image
+              src={imageUrl}
+              alt={`Image for ${title}`}
+              width={400}
+              height={400}
+              className="object-cover rounded-lg"
+            />
+          </div>
+        )}
       </div>
-      {imageUrl && (
-        <img
-          src={imageUrl}
-          alt={`Image for ${title}`}
-          className="w-full h-64 object-cover object-center mb-4"
-        />
-      )}
-      <p className="text-gray-700">{description}</p>
-    </div>
-  );
-};
+    );
+  };
 
 export default ProjectDisplay;
--- pages/past-year-projects.tsx
+++ pages/past-year-projects.tsx
@@ -1,57 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../app/globals.css';
-import Link from 'next/link';
 import Footer from "../components/footer/footer";
 import NavBar from "@/components/nav-bar/nav-bar";
-import Image from 'next/image';
-
-interface ProjectDisplayProps {
-  title: string;
-  authors: string[];
-  description: string;
-  imageUrl?: string;
-  imageOnLeft?: boolean;
-  textRightAlign?: boolean;
-}
-
-const ProjectDisplay: React.FC<ProjectDisplayProps> = ({ title, authors, description, imageUrl, imageOnLeft, textRightAlign }) => {
-    return (
-      <div className="text-white p-6 rounded-lg mb-8 flex flex-col md:flex-row">
-        {imageOnLeft && imageUrl && (
-          <div className="flex-initial mr-4">
-            <Image
-              src={imageUrl}
-              alt={`Image for ${title}`}
-              width={400}
-              height={300}
-              className="object-cover rounded-lg"
-            />
-          </div>
-        )}
-        <div className={`flex-1 ${textRightAlign ? 'text-right' : ''}`}>
-          <h3 className="text-xl font-bold mb-2">{title}</h3>
-          <div className="mb-4">
-            <strong>By:</strong> {authors.join(', ')}
-          </div>
-          <p className="text-gray-300">{description}</p>
-        </div>
-        {!imageOnLeft && imageUrl && (
-          <div className="flex-initial ml-4">
-            <Image
-              src={imageUrl}
-              alt={`Image for ${title}`}
-              width={400}
-              height={400}
-              className="object-cover rounded-lg"
-            />
-          </div>
-        )}
-      </div>
-    );
-  };
+import ProjectDisplay, { ProjectDisplayProps } from '@/components/prev-projects/projectdisplay';
 
 const PastYearProjects: React.FC = () => {
-  const projects: ProjectDisplayProps[] = [
+  const allProjects: ProjectDisplayProps[] = [
     {
       title: 'Project Title 1',
       authors: ['Name 1', 'Name 2', 'Name 3', 'Name 4'],
@@ -72,21 +26,82 @@ const PastYearProjects: React.FC = () => {
       description: 'Pellentesque habitant morbi tristique senectus et netus...',
       imageUrl: '/images/project3.jpg',
     },
+    {
+      title: 'Project Title',
+      authors: ['Name', 'Name', 'Name', 'and Name'],
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus sit amet lorem sed suscipit. Sed ultricies lacus mi, venenatis porta dui imperdiet vitae. Duis et est volutpat, hendrerit magna at, feugiat libero. Integer lacinia sed nisi venenatis feugiat. Nam pulvinar gravida felis, ut venenatis quam tincidunt ac. Maecenas et leo consectetur, mattis erat ut, varius nisi. Sed nec eros vel nunc volutpat venenatis. Duis quis ex urna. Sed pretium porta lorem, at tempor leo pellentesque ut. Donec volutpat urna at consectetur rutrum.',
+    },
+    {
+  title: 'Project Title 4',
+  authors: ['Name 1', 'Name 2', 'Name 3', 'Name 4'],
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+  imageUrl: '/images/project4.jpg',
+},
+{
+  title: 'Project Title 5',
+  authors: ['Name 1', 'Name 2', 'Name 3', 'Name 4'],
+  description: 'In tempus sit amet lorem sed suscipit...',  
+  imageUrl: '/images/project5.jpg',
+
+},
+// Repeat similar objects for projects 6 to 18
+{
+  title: 'Project Title 19',
+  authors: ['Name 1', 'Name 2', 'Name 3', 'Name 4'],
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+  imageUrl: '/images/project19.jpg',
+},
+{
+  title: 'Project Title 20',
+  authors: ['Name 1', 'Name 2', 'Name 3', 'Name 4'],
+  description: 'In tempus sit amet lorem sed suscipit...',
+  imageUrl: '/images/project20.jpg',
+
+},
   ];
 
+  // Separate the top 3 projects and the rest
+  const topProjects = allProjects.slice(0, 3);
+  const otherProjects = allProjects.slice(3);
 
-    return (
+  // State to manage the current index of the displayed project in the carousel
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // Function to navigate to the next project
+  const nextProject = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % otherProjects.length);
+  };
+
+  // Function to navigate to the previous project
+  const prevProject = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + otherProjects.length) % otherProjects.length);
+  };
+
+  return (
     <div className="container mx-auto px-4 py-8">
-        <NavBar showOnScroll={false} />
-        <h2 className="text-3xl font-bold text-center mb-8 pt-20">Previous Projects From HackRPI X</h2>
-        <div className="grid grid-cols-1 gap-8">
-        {projects.map((project, index) => (
-            <ProjectDisplay key={index} {...project} />
+      <NavBar showOnScroll={false} />
+      <h2 className="text-3xl font-bold text-center mb-8 pt-20">Previous Projects From HackRPI X</h2>
+      <div className="grid grid-cols-1 gap-8">
+        {topProjects.map((project, index) => (
+          <ProjectDisplay key={index} {...project} />
         ))}
-        </div>
-        <Footer />
+      </div>
+      <div className="flex justify-center items-center my-8">
+        <button onClick={prevProject} className="btn btn-circle btn-ghost">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7" />
+          </svg>
+        </button>
+        <ProjectDisplay {...otherProjects[currentIndex]} />
+        <button onClick={nextProject} className="btn btn-circle btn-ghost">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" />
+          </svg>
+        </button>
+      </div>
+      <Footer />
     </div>
-    );
+  );
 };
 
 export default PastYearProjects;
