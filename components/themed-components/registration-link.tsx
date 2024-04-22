f9e2d851682f126ecde2d0c869f8aef88e8db993
Cooper Werner <cooperwerner824@gmail.com>
2024-04-21 22:56:15 -0400

    updating the design for the above the fold

---
+++ components/themed-components/registration-link.tsx
@@ -0,0 +1,7 @@
+import HackRPILink from "./hackrpi-link";
+
+export default function RegistrationLink() {
+    return (
+        <HackRPILink href="https://hackrpi.com" className="text-xl"> Register Now </HackRPILink>
+    );
+}
--- components/title-components/title-text.tsx
+++ components/title-components/title-text.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import RegistrationButton from "./registration-button";
+import RegistrationButton from "@/components/themed-components/registration-link";
 
 export default function TitleText() {
 	const [year, setYear] = useState(1824);
@@ -30,15 +30,9 @@ export default function TitleText() {
 		<div className="w-full font-sans  text-left flex items-start justify-center flex-col mb-8">
 			<h1 className="w-full text-5xl 2xs:text-7xl sm:text-8xl font-bold">HACKRPI</h1>
 			<h2 className="w-full text-4xl sm:text-[4rem] whitespace-nowrap h-14">Urban Upgrades</h2>
-			<p className="w-full text-3xl sm:text-4xl mb-8 ">
-				November 9-10, <span onMouseEnter={() => setYear(1824)}>{year}</span>
+			<p className={`w-full text-3xl sm:text-4xl mb-8`}>
+				November 9-10, <span onMouseEnter={() => setYear(1824)} className={year < 2024 ? "text-[#d6001c]": "text-base-content"} >{year}</span>
 			</p>
-			<p className="w-full text-md mb-8 ">
-				HackRPI is Rensselaer Polytechnic Institute&apos;s flagship annual hackathon. This year we are bringing together
-				innovators, creators, and problem-solvers to tackle the challenges of urban living. Join us this November for a
-				weekend of free food, networking, and the opportunity to our enhance cities and communities!
-			</p>
-			{/* TODO: Replace this with the themed button when that PR is merged in. */}
 			<RegistrationButton />
 		</div>
 	);
