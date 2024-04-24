3160de8f12a7e1b059f3fdb28281925dbe2e0c6f
sdhoat <121777913+sdhoat@users.noreply.github.com>
2024-04-24 19:37:57 -0400

    Pull Updates

--- pages/maps.tsx
+++ pages/maps.tsx
@@ -2,9 +2,8 @@ import React, { useState } from "react";
 import DCC from "./maps_img/DCC.png";
 import LOW from "./maps_img/LOW.png";
 import NavBar from "@/components/nav-bar/nav-bar";
-import Head from "next/head";
+import NextImg from "next/image";
 import Footer from "@/components/footer/footer";
-import MlhBanner from "@/components/mlh-banner/mlh-banner";
 
 const MapsPage = () => {
 	const [showDCC, setShowDCC] = useState(true);
@@ -22,36 +21,44 @@ const MapsPage = () => {
 
 	return (
 		<div>
-			<Head>
-				<title>Maps Page</title>
-			</Head>
-			<MlhBanner />
 			<NavBar showOnScroll={false} />
 			<div>
 				<h1 className="title text-5xl py-10"></h1>
 			</div>
 			<div id="about" className="MapsPage text-center mx-auto w-2/3">
-				<h1 className="title font-sans-helvetica font-bold text-hackrpi-secondary-grey text-7xl py-6">MAPS</h1>
-				<div className="flex flex-col items-center space-y-1">
-					<button
-						onClick={handleShowDCC}
-						className={`bg-hackrpi-secondary-light-blue hover:bg-hackrpi-primary-light-green text-hackrpi-secondary-grey font-bold font-sans-helvetica text-2xl py-6 px-24 rounded-full mb-2 ${showDCC ? "bg-hackrpi-primary-dark-green border-hackrpi-secondary-grey border-4" : ""}`}
-					>
-						Darrin Communications Center
-					</button>
+    			<h1 className="title font-sans-helvetica font-bold text-hackrpi-secondary-grey text-7xl py-6">MAPS</h1>
+    			<div className="flex flex-row items-center space-x-4">
+        			<button
+            			onClick={handleShowDCC}
+            			className={`bg-hackrpi-secondary-light-blue hover:bg-hackrpi-primary-light-green text-hackrpi-secondary-grey font-bold font-sans-helvetica text-xl py-6 px-24 rounded-full mb-2 ${showDCC ? "bg-hackrpi-primary-dark-green border-hackrpi-secondary-grey border-4" : ""}`}
+       				>
+            			Darrin Communications Center
+        			</button>
 
-					<button
-						onClick={handleShowLOW}
-						className={`bg-hackrpi-secondary-light-blue hover:bg-hackrpi-primary-light-green text-hackrpi-secondary-grey font-bold font-sans-helvetica text-2xl py-6 px-24 rounded-full mb-2 ${showLOW ? "bg-hackrpi-primary-dark-green border-hackrpi-secondary-grey border-4" : ""}`}
-					>
-						Low Center for Industrial Innovation
-					</button>
-				</div>
+        			<button
+            			onClick={handleShowLOW}
+            			className={`bg-hackrpi-secondary-light-blue hover:bg-hackrpi-primary-light-green text-hackrpi-secondary-grey font-bold font-sans-helvetica text-xl py-6 px-24 rounded-full mb-2 ${showLOW ? "bg-hackrpi-primary-dark-green border-hackrpi-secondary-grey border-4" : ""}`}
+        			>
+           				Low Center for Industrial Innovation
+       				</button>
+    			</div>
 
 				<div className="maps-container mt-4 mb-8">
-					{showDCC && <img src={DCC.src} alt="DCC Image" className="img-fluid mx-auto" style={{ maxWidth: "100%" }} />}
+				{showDCC && <NextImg 
+					src={DCC.src} 
+					alt="DCC Image" 
 
-					{showLOW && <img src={LOW.src} alt="LOW Image" className="img-fluid mx-auto" style={{ maxWidth: "100%" }} />}
+					width = {1000}
+					height = {1000}
+					style={{ maxWidth: "100%" }} />
+				}
+				{showLOW && <NextImg 
+					src={LOW.src} 
+					alt="LOW Image"
+					width = {1000}
+					height = {1000}
+					style={{ maxWidth: "100%" }} />
+				}
 				</div>
 			</div>
 			<Footer />
