7ab74a930332532b3bb92b0d28842c0c6596bc5c
sdhoat <sdhoat@users.noreply.github.com>
2024-04-24 23:17:37 +0000

    Prettified Code!

--- pages/not-found.tsx
+++ pages/not-found.tsx
@@ -1,22 +1,22 @@
 import React, { useEffect } from "react";
 
 function NotFoundPage() {
-  useEffect(() => {
-    // This useEffect will only run on the client-side
-    console.log("Component mounted on the client-side");
-    return () => {
-      console.log("Component unmounted on the client-side");
-    };
-  }, []);
+	useEffect(() => {
+		// This useEffect will only run on the client-side
+		console.log("Component mounted on the client-side");
+		return () => {
+			console.log("Component unmounted on the client-side");
+		};
+	}, []);
 
-  // Dynamically import Custom404 only on the client-side
-  const DynamicCustom404 = React.lazy(() => import("@/pages/404"));
+	// Dynamically import Custom404 only on the client-side
+	const DynamicCustom404 = React.lazy(() => import("@/pages/404"));
 
-  return (
-    <React.Suspense fallback={<div>Loading...</div>}>
-      <DynamicCustom404 />
-    </React.Suspense>
-  );
+	return (
+		<React.Suspense fallback={<div>Loading...</div>}>
+			<DynamicCustom404 />
+		</React.Suspense>
+	);
 }
 
 export default NotFoundPage;
