45b87362112a575322fd504de9f9c82f586dedbb
sdhoat <sdhoat@users.noreply.github.com>
2024-04-24 23:38:15 +0000

    Prettified Code!

--- pages/maps.tsx
+++ pages/maps.tsx
@@ -26,39 +26,26 @@ const MapsPage = () => {
 				<h1 className="title text-5xl py-10"></h1>
 			</div>
 			<div id="about" className="MapsPage text-center mx-auto w-2/3">
-    			<h1 className="title font-sans-helvetica font-bold text-hackrpi-secondary-grey text-7xl py-6">MAPS</h1>
-    			<div className="flex flex-row items-center space-x-4">
-        			<button
-            			onClick={handleShowDCC}
-            			className={`bg-hackrpi-secondary-light-blue hover:bg-hackrpi-primary-light-green text-hackrpi-secondary-grey font-bold font-sans-helvetica text-xl py-6 px-24 rounded-full mb-2 ${showDCC ? "bg-hackrpi-primary-dark-green border-hackrpi-secondary-grey border-4" : ""}`}
-       				>
-            			Darrin Communications Center
-        			</button>
+				<h1 className="title font-sans-helvetica font-bold text-hackrpi-secondary-grey text-7xl py-6">MAPS</h1>
+				<div className="flex flex-row items-center space-x-4">
+					<button
+						onClick={handleShowDCC}
+						className={`bg-hackrpi-secondary-light-blue hover:bg-hackrpi-primary-light-green text-hackrpi-secondary-grey font-bold font-sans-helvetica text-xl py-6 px-24 rounded-full mb-2 ${showDCC ? "bg-hackrpi-primary-dark-green border-hackrpi-secondary-grey border-4" : ""}`}
+					>
+						Darrin Communications Center
+					</button>
 
-        			<button
-            			onClick={handleShowLOW}
-            			className={`bg-hackrpi-secondary-light-blue hover:bg-hackrpi-primary-light-green text-hackrpi-secondary-grey font-bold font-sans-helvetica text-xl py-6 px-24 rounded-full mb-2 ${showLOW ? "bg-hackrpi-primary-dark-green border-hackrpi-secondary-grey border-4" : ""}`}
-        			>
-           				Low Center for Industrial Innovation
-       				</button>
-    			</div>
+					<button
+						onClick={handleShowLOW}
+						className={`bg-hackrpi-secondary-light-blue hover:bg-hackrpi-primary-light-green text-hackrpi-secondary-grey font-bold font-sans-helvetica text-xl py-6 px-24 rounded-full mb-2 ${showLOW ? "bg-hackrpi-primary-dark-green border-hackrpi-secondary-grey border-4" : ""}`}
+					>
+						Low Center for Industrial Innovation
+					</button>
+				</div>
 
 				<div className="maps-container mt-4 mb-8">
-				{showDCC && <NextImg 
-					src={DCC.src} 
-					alt="DCC Image" 
-
-					width = {1000}
-					height = {1000}
-					style={{ maxWidth: "100%" }} />
-				}
-				{showLOW && <NextImg 
-					src={LOW.src} 
-					alt="LOW Image"
-					width = {1000}
-					height = {1000}
-					style={{ maxWidth: "100%" }} />
-				}
+					{showDCC && <NextImg src={DCC.src} alt="DCC Image" width={1000} height={1000} style={{ maxWidth: "100%" }} />}
+					{showLOW && <NextImg src={LOW.src} alt="LOW Image" width={1000} height={1000} style={{ maxWidth: "100%" }} />}
 				</div>
 			</div>
 			<Footer />
