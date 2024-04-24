49077b8a745179003f3b942966dd6525a4f63a56
sdhoat <121777913+sdhoat@users.noreply.github.com>
2024-04-24 19:14:37 -0400

    Pull 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import logo from "@/public/HackRPI-logo-blue.png";
 import NextImg from "next/image";
 import NavBarLink from "@/components/nav-bar/nav-bar-link";
@@ -6,18 +7,28 @@ import NavBar from "@/components/nav-bar/nav-bar";
 import "../app/globals.css";
 
 export default function Custom404() {
+    const [isHovered, setIsHovered] = useState(false);
+
     return (
         <div className="flex flex-col items-start desktop:items-center justify-start">
             <NavBar showOnScroll={false} />
-            <div className="error-message text-center mt-10">
-                <h1 className="text-4xl font-bold text-red-500">Error - 404: No page</h1>
-                <p>Sorry, the page you're looking for could not be found.</p>
+            <div className="error-message text-center mt-40">
+                <h1 className="text-4xl font-bold text-white">ERROR 404: No page found</h1>
+                <p>Click Image below to be transported back to the Home Page.</p>
             </div>
-            <div className="mt-10">
+            <div className="mt-8 flex justify-center">
                 <NavBarLink href="/">
-                    <NextImg alt="HackRPI Logo" aria-label="Home Page" src={logo} className="w-18 image-full" />
+                    <NextImg
+                        alt="HackRPI Logo"
+                        aria-label="Home Page"
+                        src={logo}
+                        className={`w-36 transition duration-300 ease-in-out ${isHovered ? 'transform scale-110' : ''}`}
+                        onMouseEnter={() => setIsHovered(true)}
+                        onMouseLeave={() => setIsHovered(false)}
+                    />
                 </NavBarLink>
             </div>
+            <div className="mt-40"></div>
             <Footer />
         </div>
     );
