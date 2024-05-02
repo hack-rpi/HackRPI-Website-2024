89508f58782287f8455a9d83b6b9f8161805796c
CooperW824 <CooperW824@users.noreply.github.com>
2024-05-02 23:49:30 +0000

    Prettified Code!

--- components/about-us.tsx
+++ components/about-us.tsx
@@ -58,8 +58,8 @@ export default function AboutUs() {
 							<p>Rensselaer Polytechnic Institute</p>
 							<p>Darrin Communications Center</p>
 						</div>
-						<div className="w-fit pl-2 md:pl-0 lg:pl-2 mb-4 md:mb-0 lg:mb-4" >
-							<RegistrationLink  />
+						<div className="w-fit pl-2 md:pl-0 lg:pl-2 mb-4 md:mb-0 lg:mb-4">
+							<RegistrationLink />
 						</div>
 					</div>
 				</div>
