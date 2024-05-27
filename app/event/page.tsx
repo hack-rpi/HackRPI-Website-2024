06cde777826cf93282f6cddad0e773f6c65e3af0
Nikul Patel <pateln14@rpi.edu>
2024-05-26 21:43:23 -0400

    added content for event page

--- app/event/page.tsx
+++ app/event/page.tsx
@@ -20,8 +20,52 @@ function EventPage() {
 			</Head>
 
 			<NavBar showOnScroll={false} />
-            <MapsPage />
 
+            <div className="justify-center text-center flex w-full items-center flex-col">
+
+				<div className="pt-20">
+					<div className="container mx-auto container mx-auto p-4">
+						<h1 className="p-2 font-medium text-2xl text-hackrpi-secondary-yellow">LOCATION OF THE EVENT</h1>
+						<div className="text-lg">
+							
+							<strong>📍 Darrin Communications Center</strong> @ Rensselaer Polytechnic Institute
+							<br />
+							<p>110 8th St, Troy, NY 12180</p>
+						</div>
+					</div>
+				</div>
+					
+				<MapsPage />
+
+				<div className="container mx-auto p-4">
+					<h1 className="p-4 font-medium text-2xl text-hackrpi-secondary-yellow">JUDGING CRITERIA</h1>
+					<ul className="text-lg">
+						<li><strong>Creativity:</strong> How original is your hack? Is this a novel idea or something that many people have come across?</li>
+						<li><strong>Practical & Useful:</strong> What problem do you want to solve? How applicable is your hack to problems we're facing today?</li>
+						<li><strong>Technical Difficulty:</strong> How technically challenging is it? Which technologies did you use?</li>
+						<li><strong>User Experience:</strong> What impression do you get from the hack? Does it provide for a smooth user experience?</li>
+						<li><strong>Future Plans:</strong> Does it have ways it can be improved upon for the future?</li>
+					</ul>
+				</div>
+
+				<div className="container mx-auto container mx-auto p-4">
+					<h1 className="p-4 font-medium text-2xl text-hackrpi-secondary-yellow">PROJECT SUBMISSION</h1>
+					<p className="text-lg">Once you have your project ready, Submit your project on Devpost and give a brief presentation at our showcase. You can edit your Devpost submission until the deadline.</p>
+				</div>
+
+				<div className="container mx-auto container mx-auto p-4">
+					<h1 className="p-4 font-medium text-2xl text-hackrpi-secondary-yellow">MENTORING INFORMATION</h1>
+					<p className="text-lg">Mentors will be available throughout the event to provide guidance and assistance to participants. Whether you need help with coding, debugging, or refining your project idea, our experienced mentors are here to support you every step of the way.</p>
+				</div>
+
+				<div className="container mx-auto container mx-auto p-4">
+					<h1 className="p-4 font-medium text-2xl text-hackrpi-secondary-yellow">EVENT DISCORD</h1>
+					<p className="text-lg mb-8">
+						Get involved and stay updated! Join our Discord server to access resources, network with others, and receive event announcements: <a href="https://discord.gg/phCzaMr4" className="text-blue-500 hover:underline">Join Discord</a>
+					</p>
+				</div>
+
+            </div>
 
 			<Footer />
 		</>
--- app/location/page.tsx
+++ app/location/page.tsx
@@ -21,23 +21,20 @@ const MapsPage = () => {
 	};
 
 	return (
-		<div className="flex w-full items-center justify-center flex-col">
-			<div>
-				<h1 className="title text-5xl py-10"></h1>
-			</div>
+		<div className="p-2 flex flex-col items-center justify-center">
 			<div
 				id="maps"
-				className="text-center flex items-center justify-center flex-col w-11/12 md:w-2/3  h-[calc(100vh-80px)]"
+				className="text-center flex items-center justify-center flex-col w-11/12 md:w-2/3"
 			>
-				<h1 className="font-sans font-bold text-hackrpi-secondary-grey text-7xl ">MAPS</h1>
+				<h1 className="font-medium text-2xl text-hackrpi-secondary-yellow">MAPS</h1>
 
-				<div className="mt-4 mb-8 max-w-[800px]">
+				<div className="mt-2 mb-2 max-w-[800px]">
 					<div className="flex flex-col md:flex-row items-center justify-between w-full md:space-x-4 md:space-y-0 space-y-4 mb-4">
-						<HackRPIButton onClick={handleShowDCC} active={showDCC} className="text-xl md:w-[350px] w-full ">
+						<HackRPIButton onClick={handleShowDCC} active={showDCC} className="text-xl md:w-[350px] w-full">
 							Darrin Communications Center
 						</HackRPIButton>
 
-						<HackRPIButton onClick={handleShowLOW} active={showLOW} className="text-xl md:w-[350px] w-full ">
+						<HackRPIButton onClick={handleShowLOW} active={showLOW} className="text-xl md:w-[350px] w-full">
 							Low Center for Industrial Innovation
 						</HackRPIButton>
 					</div>
