a7f2e0bf053b41692854eba911a1ac8f76dbdeaf
Jordan Ye <79342877+Jordan231111@users.noreply.github.com>
2024-06-04 16:50:45 -0400

    Fixed alignment and rendering performance

--- pages/past-year-projects.tsx
+++ pages/past-year-projects.tsx
@@ -7,37 +7,31 @@ import Arrow from "@/components/prev-projects/Arrow";
 
 const PastYearProjects: React.FC = () => {
 	const allProjects: ProjectDisplayProps[] = [
-		{
-			title: "DAVE",
-			authors: ["Name 1", "Name 2", "Name 3", "Name 4"],
-			description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
-			imageUrl: "/projectImages/firstPlaceHack.png",
-		},
-		{
-			title: "HealthChat",
-			authors: ["Name 1", "Name 2", "Name 3", "Name 4"],
-			description: "In tempus sit amet lorem sed suscipit...",
-			imageUrl: "/projectImages/secondPlaceHack.png  ",
-			imageOnLeft: true,
-			textRightAlign: true,
-		},
+{
+  title: "DAVE",
+  authors: ["Aryash", "Zachary Fernandes", "Drew Bhavsar"],
+  description: "We used LLaVA, Whisper, and TTS to create a conversational, multimodal AI assistant for the visually impaired. Users provide images and questions, and the AI describes images and answers questions. This project places a camera above the user's left eye on a pair of glasses to take pictures of the world around the user. The user can summon the assistant by saying 'DAVE'. The user can ask DAVE to describe the image of the world around them. Alternatively, the user can ask DAVE questions about their surroundings. The camera takes a picture from its perspective. The user's speech is interpreted as text and combined with the image to prompt the model. The model provides an appropriate response to the user through speech.",
+  imageUrl: "/projectImages/firstPlaceHack.png",
+},
+{
+  title: "HealthChat",
+  authors: ["Michael Anderson"],
+  description: "HealthChat uses AI to answer patient questions to help them better find a doctor that suites their needs! Gone are the days of endless searching when you need the services of a medical professional. With HealthChat you can ask questions about your medical issue and be provided with doctors in your area that match your criteria. Using the Bing GPT4 Engine, HealthChat is able to search the internet for you, using the most reliable sources first.",
+  imageUrl: "/projectImages/secondPlaceHack.png",
+  imageOnLeft: true,
+  textRightAlign: true,
+},
 		{
 			title: "RPillPal: A Biometric Pill Dispenser",
-			authors: ["Name 1", "Name 2", "Name 3", "Name 4"],
-			description: "Pellentesque habitant morbi tristique senectus et netus...",
+			authors: ["Dan Fiumara", "Om Anavekar", "Abdul Muizz", "Amaan Qureshi"],
+			description: "RPillPal is a biometric-based pill dispenser designed for home use. Its goal is to utilize biometric fingerprint data to determine patient identity and supply the correct dose based on doctor recommendations. It connects directly with a patient's healthcare providers, allowing them to monitor progress and make timely adjustments to treatment plans.",
 			imageUrl: "/projectImages/patientsafety.png",
 		},
 		{
-			title: "Project Title",
-			authors: ["Name", "Name", "Name", "and Name"],
-			description:
-				"Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus sit amet lorem sed suscipit. Sed ultricies lacus mi, venenatis porta dui imperdiet vitae. Duis et est volutpat, hendrerit magna at, feugiat libero. Integer lacinia sed nisi venenatis feugiat. Nam pulvinar gravida felis, ut venenatis quam tincidunt ac. Maecenas et leo consectetur, mattis erat ut, varius nisi. Sed nec eros vel nunc volutpat venenatis. Duis quis ex urna. Sed pretium porta lorem, at tempor leo pellentesque ut. Donec volutpat urna at consectetur rutrum.",
-		},
-		{
-			title: "Project Title 4",
+			title: "Ed-You",
 			authors: ["Name 1", "Name 2", "Name 3", "Name 4"],
-			description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
-			imageUrl: "/images/project4.jpg",
+			description: "Ed-You is a web application that creates a direct channel for secure feedback from students about teachers to administration. Ed-You is catered to two main audiences: students and administrators. Students enter their school code, which is randomly generated and available to the admin in their school, and are able to choose from a list of teachers at their school and provide anonymous feedback whenever they want. The only restriction to student-feedback is toxicity: the application uses a fine-tuned large language model to flag any potentially harmful comments and force the student to reword them. On the administration side, admins can log in through Google OAuth to access their dashboard. A list of teachers from their school will be visible, and admins can click on any teacher to learn more about the feedback given to them. This feedback takes the form of an average rating calculated by another fine-tuned LLM, an AI-generated summary, and raw comments with time stamps.",
+			imageUrl: "/projectImages/AIHack.png",
 		},
 		{
 			title: "Project Title 5",
@@ -72,35 +66,37 @@ const PastYearProjects: React.FC = () => {
 		setCurrentIndex((prevIndex) => (prevIndex - 1 + otherProjects.length) % otherProjects.length);
 	};
 
-	return (
-		<div>
-			<div className="container mx-auto px-4 py-8 max-w-screen-lg">
-				<NavBar showOnScroll={false} />
-				<h2 className="text-3xl font-bold text-center mb-8 pt-20">Previous Projects From HackRPI X</h2>
-				<div className="grid grid-cols-1 gap-8 mx-auto">
-					{topProjects.map((project, index) => (
-						<div key={index}>
-							<ProjectDisplay {...project} />
-							{index < topProjects.length - 1 && <hr className="my-8 border-t border-gray-200 w-1/2 mx-auto" />}{" "}
-							{/* Add this line */}
-						</div>
-					))}
-				</div>
-				<hr className="my-8 border-t border-gray-200 w-1/2 mx-auto" /> {/* Add this line */}
-				<div className="flex justify-center items-center my-8 mx-auto">
-					<div className="bg-gray-200 p-4">
-						<Arrow direction="left" onClick={prevProject} />
-					</div>
-					<ProjectDisplay {...otherProjects[currentIndex]} />
-					<div className="bg-gray-200 p-4">
-						<Arrow direction="right" onClick={nextProject} />
-					</div>
-				</div>
-				<hr className="my-8 border-t border-gray-200 w-1/2 mx-auto" /> {/* Add this line */}
-			</div>
-			<Footer />
-		</div>
-	);
+return (
+  <div>
+    <div className="container mx-auto px-4 py-8 max-w-screen-lg pb-60">
+      <NavBar showOnScroll={false} />
+      <h2 className="text-3xl font-bold text-center mb-8 pt-20">Previous Projects From HackRPI X</h2>
+      <div className="grid grid-cols-1 gap-8 mx-auto">
+        {topProjects.map((project, index) => (
+          <div key={index}>
+            <ProjectDisplay {...project} />
+            {index < topProjects.length - 1 && <hr className="my-8 border-t border-gray-200 w-1/2 mx-auto" />}
+          </div>
+        ))}
+      </div>
+      <hr className="my-8 border-t border-gray-200 w-1/2 mx-auto" /> {/* Moved the horizontal bar here */}
+      <div className="flex justify-center items-center my-8 mx-auto h-96">
+<div className="flex items-center bg-gray-200 p-4" style={{ height: '70%' }}>
+  <Arrow direction="left" onClick={prevProject} />
+</div>
+<div className="flex flex-col items-center h-full mx-8"> {/* Added horizontal margins here */}
+  <img src={otherProjects[currentIndex].imageUrl} alt={otherProjects[currentIndex].title} className="w-full h-full object-cover" />
+</div>
+<div className="flex items-center bg-gray-200 p-4" style={{ height: '70%' }}>
+  <Arrow direction="right" onClick={nextProject} />
+</div>
+      </div>
+      <ProjectDisplay {...otherProjects[currentIndex]} displayImage={false} />
+      <hr className="my-8 border-t border-gray-200 w-1/2 mx-auto mb-20" />
+    </div>
+    <Footer />
+  </div>
+);
 };
 
 export default PastYearProjects;
