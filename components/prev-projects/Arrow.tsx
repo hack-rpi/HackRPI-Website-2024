460d8b77124f2dcf944fd2e1f2830b6a0ef2556c
Jordan Ye <79342877+Jordan231111@users.noreply.github.com>
2024-06-05 21:50:41 -0400

    UpdatedDesignChoice and finish importing json data

--- components/prev-projects/Arrow.tsx
+++ components/prev-projects/Arrow.tsx
@@ -1,27 +1,27 @@
 import React from "react";
 
 interface ArrowProps {
-	direction: "left" | "right";
-	onClick: () => void; // Explicitly define the type of the onClick prop
+    direction: "left" | "right";
+    onClick: () => void; // Explicitly define the type of the onClick prop
 }
 
-const Arrow: React.FC<ArrowProps> = ({ direction, onClick }) => (
-	<button onClick={onClick} className="bg-gray-200 rounded-md p-2 hover:bg-gray-300 transition-colors duration-200">
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			className={`h-24 w-24 text-gray-800 ${direction === "left" ? "rotate-180" : ""}`} // Increase size to h-24 w-24
-			fill="none"
-			viewBox="0 0 24 24"
-			stroke="currentColor"
-		>
-			<path
-				strokeLinecap="round"
-				strokeLinejoin="round"
-				strokeWidth={3} // Increase strokeWidth to 3
-				d="M14 6l6 6m0 0l-6 6m6-6H3" // Modify the d attribute to make the arrow points longer
-			/>
-		</svg>
-	</button>
-);
+const Arrow: React.FC<ArrowProps> = React.memo(({ direction, onClick }) => (
+    <button onClick={onClick} className="bg-gray-200 rounded-md p-2 hover:bg-gray-300 transition-colors duration-200">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={`h-24 w-24 text-gray-800 ${direction === "left" ? "rotate-180" : ""}`} // Increase size to h-24 w-24
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+        >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={3} // Increase strokeWidth to 3
+                d="M14 6l6 6m0 0l-6 6m6-6H3" // Modify the d attribute to make the arrow points longer
+            />
+        </svg>
+    </button>
+));
 
 export default Arrow;
--- pages/past-year-projects.tsx
+++ pages/past-year-projects.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import "../app/globals.css";
 import Footer from "../components/footer/footer";
 import NavBar from "@/components/nav-bar/nav-bar";
@@ -38,23 +38,83 @@ const PastYearProjects: React.FC = () => {
 			imageUrl: "/projectImages/AIHack.png",
 		},
 		{
-			title: "Project Title 5",
-			authors: ["Name 1", "Name 2", "Name 3", "Name 4"],
-			description: "In tempus sit amet lorem sed suscipit...",
-			imageUrl: "/images/project5.jpg",
+			title: "PyPlay",
+			authors: ["Jeremy Goldberger", "Nyssa Gandhi", "Floofer++ Rafael", "zcacciapalle"],
+			description: "PyPlay ports through the basic features of Scratch via PyGame and handles all of the multithreading behind the scenes so that kids can learn to code easily, continuing making Scratch-like games while learning to write in a full-fledged programming language.",
+			imageUrl: "/projectImages/EducationHack.png",
 		},
 		// Repeat similar objects for projects 6 to 18
 		{
-			title: "Project Title 19",
-			authors: ["Name 1", "Name 2", "Name 3", "Name 4"],
+			title: "CryptoLearner",
+			authors: ["Hunter Gould", "smullahy", "Ashlesha Bhagat", "Name 4"],
 			description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
-			imageUrl: "/images/project19.jpg",
+			imageUrl: "/projectImages/cyber.png",
 		},
 		{
-			title: "Project Title 20",
-			authors: ["Name 1", "Name 2", "Name 3", "Name 4"],
-			description: "In tempus sit amet lorem sed suscipit...",
-			imageUrl: "/images/project20.jpg",
+			title: "Native Lawn",
+			authors: ["Trinity Gagnon", "Ethan Cheon", "Oliver Centner"],
+			description: "A straightforward website that helps people swap their lawns for environmentally friendly native alternatives by providing visual resources and information. Our site serves as a one-stop resource for learning about the cons of lawns, and what alternatives are available. Planting native plants encourages biodiversity and diminishes the need for watering, pesticide use, and other resource-reliant or damaging tools. Native plants vary greatly by area, even only within the U.S.A., so our site provides a visual guide to what lawn alternatives are native to where, via an interactive map. Further details on how to swap and curate your lawn are then provided, with Native Lawn acting as an every-step guide to swapping your lawn.",
+			imageUrl: "/projectImages/unNamed.png",
+		},
+		{
+			title: "¢Track (Every Cent Counts)",
+			authors: ["Jimi Xia", "Nicole Spaul", "Jase Krebs", "Lilu K"],
+			description: "Intakes the prospective financial plan from the user in the form of a slider showing savings to spending ratio (also intakes users source of income, or if they are a homemaker or elderly). Intakes the transactions of the user from their service of choice (Paypal, Bank, etc.). Tracts the categories of transactions, outputs user spending habits in the form of percentages and graphs. Makes suggestions off the previously found data on how to better adhere to the users plan of finances. Also makes suggestions for financial plans/ future plans based on the users determined source of income, and their predetermined set plan when they first downloaded the app.",
+			imageUrl: "/projectImages/unNamed.png",
+		},
+		{
+			title: "Student Health Prediction Model",
+			authors: ["Felipe Alves", "Sofia Carmona", "David Quintero", "Brina Bhagat"],
+			description: "In this model, we take in a CSV file of students at the college level. This Data was provided by the America On Tech organization, we used their data. The data is categorized into an ID to prevent any data from the same student, gender, major, GPA, class status, marital status, whether or not they have depression, whether or not they have anxiety, whether or not they have panic attacks, and if they've sought treatment previously. Using this data we created a Principle Component Analysis and created a Machine Learning Model to predict the number of students who received help and how many didn't.",
+			imageUrl: "/projectImages/datascience.png",
+		},
+		{
+			title: "HelpAlign",
+			authors: ["Haddleton"],
+			description: "HelpAlign helps you connect with mental health providers in your area, all with the click of a button. We're not a help line - we help you, and your help, align.",
+			imageUrl: "/projectImages/unNamed.png",
+		},
+		{
+			title: "PictoPlan",
+			authors: ["Yash Kaul", "Crizbae", "Jason Chen", "Ryan Cole"],
+			description: "PictoPlan lets the user select their own photos of content from books or textbooks and upload them to our site. After this happens, the photo is converted into text using OCR and then passed through OpenAI's API in order to summarize and create a lesson plan for the respective content. This lesson plan is then displayed on the website, and is accessible through a permanent link to facilitate teachers sharing lesson-plans and collaborating.",
+			imageUrl: "/projectImages/firstTime.png",
+		},
+		{
+			title: "Augma",
+			authors: ["Sadashiva Yalla", "Liam Banahan", "Shrujanus Mutheboyina"],
+			description: "Introducing Augma, the groundbreaking app that leverages the power of artificial intelligence to transform the world of accessibility. Our innovative solution empowers individuals with disabilities by automating text-to-speech like never before. With Augma, we've reimagined inclusivity for a digital age, making any text accessible in an instant. Imagine a world where barriers to communication are shattered with a simple tap on your smartphone. Augma effortlessly converts images of text into clear, natural-sounding speech. Whether you have a learning disability, are visually impaired, or just looking for a convenient way to consume written content, our AI-driven app ensures everyone can access information independently. Join us in revolutionizing accessibility and ensuring that everyone has the power to hear the world around them.",
+			imageUrl: "/projectImages/googlecloudusage.png",
+		},
+		{
+			title: "EmergencyRoom.Compare",
+			authors: ["Eden Bendheim", "Toby McDonald", "Viane Matsibekker", "Michael Halpern"],
+			description: "In some cases when going to the ER, it may be faster to go to a further away ER to avoid waiting time and be seen faster in net time. Using our tool, we will find you the ERs that will see you the soonest based on a combination of the time to drive to the ER and the estimated ER waiting time.",
+			imageUrl: "/projectImages/googlecloudusage2.png",
+		},
+		{
+			title: "GitHub IDE",
+			authors: ["Momir-Petrovic Petrovic", "Advait"],
+			description: "Our project allows you to run your code straight from the github repository readme file. Whenever the GitHub repository has a commit, it takes the readme, compiles the code and adds the output to the end of the readme",
+			imageUrl: "/projectImages/unNamed.png",
+		},
+		{
+			title: "Terra",
+			authors: ["Liam Agu Vaitkus", "Bigmaccar McCarthy", "Lillith", "Christian Monfiston"],
+			description: 'Terra, named after the goddess of nature, or, "Mother Earth," seeks to connect humanitarian organizations with volunteers who are eager to lend a hand to communities impacted by tragedy. Organizations can post volunteer openings and our site facilitates the rest.',
+			imageUrl: "/projectImages/domainName.png",
+		},
+		{
+			title: "SparkSwap",
+			authors: ["Joe Schmoe", "Tripp Lyons"],
+			description: "SparkSwap allows users to buy electricity tokens either directly from energy companies in advance or purchase from traders. These tokens are for a certain amount of electricity produced in a certain manner at a particular time. These tokens are then redeemed for actual electricity from the company. The ability of a user to directly choose how much power of each particular type they want is a strength of this system. Additionally, buying a particular amount of electricity from the energy company ahead of time reduces the risk for the energy company and the price for the consumer, thus both have an incentive to use the service.",
+			imageUrl: "/projectImages/web3.png",
+		},
+		{
+			title: "Graphing Guinea",
+			authors: ["Hanna Nakajima", "Kevin Liu", "Claire Chen", "MEKNOWKNOOB Lee"],
+			description: "Our game uses puzzle-solving to make learning fun. Each level begins with a guinea pig trying to traverse a gap to the other side of the room. With the player's inputted equations and limits, the guinea pig's tunnel is created allowing it to successfully reach the end goal.",
+			imageUrl: "/projectImages/bestGame.png",
 		},
 	];
 
@@ -76,31 +136,31 @@ const PastYearProjects: React.FC = () => {
 		}
 	}, []);
 
-const nextProject = () => {
-    setCurrentIndex(prevIndex => {
-        const nextIndex = (prevIndex + 1) % otherProjects.length;
-        // Preload next image
-        const nextNextIndex = (nextIndex + 1) % otherProjects.length;
-        const img = new Image();
-		if (otherProjects[nextNextIndex].imageUrl) {
+	const nextProject = useCallback(() => {
+		setCurrentIndex(prevIndex => {
+		  const nextIndex = (prevIndex + 1) % otherProjects.length;
+		  // Preload next image
+		  const nextNextIndex = (nextIndex + 1) % otherProjects.length;
+		  const img = new Image();
+		  if (otherProjects[nextNextIndex].imageUrl) {
 			img.src = otherProjects[nextNextIndex].imageUrl || '';
-		}
-        return nextIndex;
-    });
-};
-
-const prevProject = () => {
-    setCurrentIndex(prevIndex => {
-        const newIndex = (prevIndex - 1 + otherProjects.length) % otherProjects.length; // Renamed to newIndex
-        // Preload previous image
-        const prevPrevIndex = (newIndex - 1 + otherProjects.length) % otherProjects.length; // Use newIndex here
-        const img = new Image();
-		if (otherProjects[prevPrevIndex].imageUrl) {
+		  }
+		  return nextIndex;
+		});
+	  }, [otherProjects]);
+	
+	  const prevProject = useCallback(() => {
+		setCurrentIndex(prevIndex => {
+		  const newIndex = (prevIndex - 1 + otherProjects.length) % otherProjects.length; // Renamed to newIndex
+		  // Preload previous image
+		  const prevPrevIndex = (newIndex - 1 + otherProjects.length) % otherProjects.length; // Use newIndex here
+		  const img = new Image();
+		  if (otherProjects[prevPrevIndex].imageUrl) {
 			img.src = otherProjects[prevPrevIndex].imageUrl || '';
-		}
-        return newIndex; // Return newIndex
-    });
-};
+		  }
+		  return newIndex; // Return newIndex
+		});
+	  }, [otherProjects]);
 
 	return (
 		<div>
