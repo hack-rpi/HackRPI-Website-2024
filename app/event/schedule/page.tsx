f11217f0718bbe49770dac1f30d56d706f1684c0
Cooper Werner <cooperwerner824@gmail.com>
2024-06-08 20:17:39 -0400

    starting the schedule page, still a lot of work to do here

--- app/event/schedule/page.tsx
+++ app/event/schedule/page.tsx
@@ -3,35 +3,130 @@
 import NavBar from "@/components/nav-bar/nav-bar";
 import "@/app/globals.css";
 import Footer from "@/components/footer/footer";
-import logo from "@/public/HackRPI_Logo_Yellow_Arrow.png";
-import Image from "next/image";
+import { useEffect, useState } from "react";
+
+import { Amplify } from "aws-amplify";
+import awsconfig from "@/amplify_outputs.json";
+
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
+
+Amplify.configure(awsconfig);
+const client = generateClient<Schema>({});
+
+type Event = {
+	id: string;
+	title: string;
+	description: string;
+	startTime: number;
+	endTime: number;
+	location: string;
+	speaker: string;
+	visible: true;
+};
 
 export default function page() {
+	const [currentDateTime, setCurrentDateTime] = useState(new Date());
+	const [events, setEvents] = useState<Event[]>([]);
+	const [state, setState] = useState<"loading" | "loaded" | "error">("loading");
+	const [happeningNow, setHappeningNow] = useState<Event[]>([]);
+
+	async function fetchEvents(): Promise<Event[]> {
+		let { data, errors } = await client.models.event.list({
+			filter: { visible: { eq: true } },
+			limit: 100,
+			authMode: "identityPool",
+		});
+
+		if (!errors && data) {
+			setState("loaded");
+			return data.map((event) => event as Event);
+		}
+
+		// If the user is authenticated and apart of the directors group, they should be able to see all events
+		// For some reason, amplify is failing to use the Authenticated IAM role, for directors so we will use the userPool role instead
+
+		({ data, errors } = await client.models.event.list({
+			filter: { visible: { eq: true } },
+			limit: 100,
+			authMode: "userPool",
+		}));
+
+		if (!errors && data) {
+			setState("loaded");
+			return data.map((event) => event as Event);
+		}
+
+		setState("error");
+		console.error(errors);
+		return [];
+	}
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCurrentDateTime(new Date());
+		}, 1000);
+
+		fetchEvents().then((events) => {
+			setEvents(events);
+			setHappeningNow(determineHappeningNow(events));
+		});
+
+		return () => clearInterval(interval);
+	}, []);
+
 	return (
 		<div className="flex flex-col w-screen h-fit min-h-screen items-center justify-center">
 			<NavBar showOnScroll={false} />
-			<div className="flex flex-col w-11/12 md:w-1/2 flex-grow flex-shrink basis-auto items-center justify-center text-center desktop:text-left">
-				<div className="h-24 desktop:h-16"></div>
-				<Image src={logo} alt="HackRPI Logo" width={100} height={100} />
-				<h1 className="text-4xl font-bold mb-4">🚧 HackRPI Site Under Development! 🚧</h1>
-				<p className="text-xl mb-2">
-					Attention all code warriors and caffeine aficionados! Our hackathon hub is currently in the middle of an epic
-					upgrade. We&apos;re coding faster than a team of over-caffeinated developers at 3 AM.
-				</p>
-				<ul className="list-disc w-full list-inside mb-2">
-					<li>Challenges so brain-bending, they&apos;ll make your algorithms sweat!</li>
-					<li>Prizes so cool, even your keyboard will do a happy dance.</li>
-					<li>Resources so valuable, they&apos;ll make Stack Overflow blush.</li>
-				</ul>
-				<p className="mb-2">
-					Stay tuned for the ultimate hackathon experience. In the meantime, feel free to practice your speed coding and
-					stock up on energy drinks. This is going to be one heck of a digital showdown.
-				</p>
-				<p className="w-full mb-2">
-					Thanks for your patience! (And for not accidentally deploying anything to production.)
-				</p>
+			<div className="w-11/12 desktop:w-2/3 flex-grow flex-shrink basis-auto mt-28 desktop:mt-16">
+				<div className="flex w-full items-center justify-between">
+					<h1 className="text-3xl xs:text-4xl font-bold text-center">Schedule</h1>
+					<p className="text-center font-bold text-xl  xs:text-3xl">
+						{currentDateTime.toLocaleString(undefined, { dateStyle: "short", timeStyle: "short" })}
+					</p>
+				</div>
+				{state === "loading" && <p className="text-3xl mb-2">Loading Schedule...</p>}
+
+				{state === "error" && (
+					<div className="badge bg-primary flex items-center justify-center h-fit my-4 ">
+						<p className="text-error-content text-xl p-2">
+							Oops! Looks like we ran into an issue loading the events. Please check your internet and refresh to try
+							again, if the problem persists, please let us know at <a href="mailto:hackrpi@rpi.edu">hackrpi@rpi.edu</a>
+							. Thank you!
+						</p>
+					</div>
+				)}
+
+				{state === "loaded" && (
+					<div>
+						{happeningNow.length > 0 && (
+							<div>
+								<h2>Happening Now!</h2>
+								{happeningNow.map((event) => (
+									<div key={event.id} className="border-b border-gray-400 h-16 flex justify-start items-center">
+										<div></div>
+										<div></div>
+										<div></div>
+									</div>
+								))}
+							</div>
+						)}
+						{events.map((event) => (
+							<div key={event.id} className="border-b border-gray-400 h-16 flex justify-start items-center">
+								<div>{event.title}</div>
+							</div>
+						))}
+					</div>
+				)}
 			</div>
 			<Footer />
 		</div>
+	);
+}
+
+function determineHappeningNow(events: Event[]): Event[] {
+	const currentDateTime = new Date();
+	return events.filter(
+		(event) => event.startTime < currentDateTime.getTime() && event.endTime > currentDateTime.getTime(),
 	);
 }
