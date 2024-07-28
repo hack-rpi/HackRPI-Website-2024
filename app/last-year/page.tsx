eeb47f13f071a216e6df703600d17f53a7591a55
Cooper Werner <cooperwerner824@gmail.com>
2024-07-28 14:21:52 -0400

    fixing the build

--- app/last-year/page.tsx
+++ app/last-year/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState, useCallback } from "react";
+import React from "react";
 import "@/app/globals.css";
 import Footer from "../../components/footer/footer";
 import NavBar from "@/components/nav-bar/nav-bar";
