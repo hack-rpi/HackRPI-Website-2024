e0839ac773461f60b3f0457073f3414de97c2126
lilujk <lilujk@users.noreply.github.com>
2024-08-04 22:51:24 +0000

    Prettified Code!

--- app/last-year/photos/page.tsx
+++ app/last-year/photos/page.tsx
@@ -14,132 +14,57 @@ export default function PhotoGallery() {
 			<div className="flex items-center justify-center">
 				<div className="grid grid-cols-8 gap-5 p-5">
 					<div className="col-span-4">
-						<Image
-							src="/lastYearPhotos/IMG_1115.jpg"
-							alt="HackRPI X 2023 Photo"
-							width={1000}
-							height={1000}
-						/>
+						<Image src="/lastYearPhotos/IMG_1115.jpg" alt="HackRPI X 2023 Photo" width={1000} height={1000} />
 					</div>
 					<div className="col-span-4 grid grid-cols-2 gap-5">
 						<div className="col-span-1">
-							<Image
-								src="/lastYearPhotos/IMG_1118.jpg"
-								alt="HackRPI X 2023 Photo"
-								width={500}
-								height={500}
-							/>
+							<Image src="/lastYearPhotos/IMG_1118.jpg" alt="HackRPI X 2023 Photo" width={500} height={500} />
 						</div>
 						<div className="col-span-1">
-							<Image
-								src="/lastYearPhotos/IMG_1123.jpg"
-								alt="HackRPI X 2023 Photo"
-								width={500}
-								height={500}
-							/>
+							<Image src="/lastYearPhotos/IMG_1123.jpg" alt="HackRPI X 2023 Photo" width={500} height={500} />
 						</div>
 						<div className="col-span-1">
-							<Image
-								src="/lastYearPhotos/IMG_1126.jpg"
-								alt="HackRPI X 2023 Photo"
-								width={500}
-								height={500}
-							/>
+							<Image src="/lastYearPhotos/IMG_1126.jpg" alt="HackRPI X 2023 Photo" width={500} height={500} />
 						</div>
 						<div className="col-span-1">
-							<Image
-								src="/lastYearPhotos/IMG_1127.jpg"
-								alt="HackRPI X 2023 Photo"
-								width={500}
-								height={500}
-							/>
+							<Image src="/lastYearPhotos/IMG_1127.jpg" alt="HackRPI X 2023 Photo" width={500} height={500} />
 						</div>
 					</div>
 					<div className="col-span-2 grid grid-rows-2 gap-5">
 						<div className="col-span-1">
-							<Image
-								src="/lastYearPhotos/IMG_1131.jpg"
-								alt="HackRPI X 2023 Photo"
-								width={500}
-								height={500}
-							/>
+							<Image src="/lastYearPhotos/IMG_1131.jpg" alt="HackRPI X 2023 Photo" width={500} height={500} />
 						</div>
 						<div className="col-span-1">
-							<Image
-								src="/lastYearPhotos/IMG_1135.jpg"
-								alt="HackRPI X 2023 Photo"
-								width={500}
-								height={500}
-							/>
+							<Image src="/lastYearPhotos/IMG_1135.jpg" alt="HackRPI X 2023 Photo" width={500} height={500} />
 						</div>
 					</div>
 					<div className="col-span-4">
-						<Image
-							src="/lastYearPhotos/IMG_1144.jpg"
-							alt="HackRPI X 2023 Photo"
-							width={1000}
-							height={1000}
-						/>
+						<Image src="/lastYearPhotos/IMG_1144.jpg" alt="HackRPI X 2023 Photo" width={1000} height={1000} />
 					</div>
 					<div className="col-span-2 grid grid-rows-2 gap-5">
 						<div className="col-span-1">
-							<Image
-								src="/lastYearPhotos/IMG_1148.jpg"
-								alt="HackRPI X 2023 Photo"
-								width={500}
-								height={500}
-							/>
+							<Image src="/lastYearPhotos/IMG_1148.jpg" alt="HackRPI X 2023 Photo" width={500} height={500} />
 						</div>
 						<div className="col-span-1">
-							<Image
-								src="/lastYearPhotos/IMG_1151.jpg"
-								alt="HackRPI X 2023 Photo"
-								width={500}
-								height={500}
-							/>
+							<Image src="/lastYearPhotos/IMG_1151.jpg" alt="HackRPI X 2023 Photo" width={500} height={500} />
 						</div>
 					</div>
 					<div className="col-span-4 grid grid-cols-2 gap-5">
 						<div className="col-span-1">
-							<Image
-								src="/lastYearPhotos/IMG_1153.jpg"
-								alt="HackRPI X 2023 Photo"
-								width={500}
-								height={500}
-							/>
+							<Image src="/lastYearPhotos/IMG_1153.jpg" alt="HackRPI X 2023 Photo" width={500} height={500} />
 						</div>
 						<div className="col-span-1">
-							<Image
-								src="/lastYearPhotos/IMG_1157.jpg"
-								alt="HackRPI X 2023 Photo"
-								width={500}
-								height={500}
-							/>
+							<Image src="/lastYearPhotos/IMG_1157.jpg" alt="HackRPI X 2023 Photo" width={500} height={500} />
 						</div>
 						<div className="col-span-1">
-							<Image
-								src="/lastYearPhotos/IMG_1162.jpg"
-								alt="HackRPI X 2023 Photo"
-								width={500}
-								height={500}
-							/>
+							<Image src="/lastYearPhotos/IMG_1162.jpg" alt="HackRPI X 2023 Photo" width={500} height={500} />
 						</div>
 						<div className="col-span-1">
-							<Image
-								src="/lastYearPhotos/IMG_1169.jpg"
-								alt="HackRPI X 2023 Photo"
-								width={500}
-								height={500}
-							/>
+							<Image src="/lastYearPhotos/IMG_1169.jpg" alt="HackRPI X 2023 Photo" width={500} height={500} />
 						</div>
 					</div>
 					<div className="col-span-4">
-						<Image
-							src="/lastYearPhotos/IMG_1179.jpg"
-							alt="HackRPI X 2023 Photo"
-							width={1000}
-							height={1000}
-						/>
+						<Image src="/lastYearPhotos/IMG_1179.jpg" alt="HackRPI X 2023 Photo" width={1000} height={1000} />
 					</div>
 				</div>
 			</div>
