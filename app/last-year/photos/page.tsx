064f8e532493f4cd3cc32aa5cb7d8663704aaede
Lilu Kaufman <kaufmanliluj@gmail.com>
2024-08-04 18:48:00 -0400

    Add inital format test

--- app/last-year/photos/page.tsx
+++ app/last-year/photos/page.tsx
@@ -5,15 +5,6 @@ import "@/app/globals.css";
 import Footer from "../../../components/footer/footer";
 import NavBar from "@/components/nav-bar/nav-bar";
 import Image from "next/image";
-import image1 from "@/public/lastYearPhotos/IMG_1115.jpg";
-import image2 from "@/public/lastYearPhotos/IMG_1118.jpg";
-import image3 from "@/public/lastYearPhotos/IMG_1123.jpg";
-import image4 from "@/public/lastYearPhotos/IMG_1126.jpg";
-import image5 from "@/public/lastYearPhotos/IMG_1127.jpg";
-import image6 from "@/public/lastYearPhotos/IMG_1131.jpg";
-import image7 from "@/public/lastYearPhotos/IMG_1135.jpg";
-import image8 from "@/public/lastYearPhotos/IMG_1144.jpg";
-import image9 from "@/public/lastYearPhotos/IMG_1148.jpg";
 
 export default function PhotoGallery() {
 	return (
@@ -21,82 +12,134 @@ export default function PhotoGallery() {
 			<NavBar showOnScroll={false} />
 			<h2 className="text-3xl font-bold text-center mb-8 pt-20">A Memorable Time in HackRPI X!</h2>
 			<div className="flex items-center justify-center">
-				<div className="grid-cols-4">
-					<div className="inline-grid">
+				<div className="grid grid-cols-8 gap-5 p-5">
+					<div className="col-span-4">
 						<Image
-							src={image1}
-							width={500}
-							height={500}
-							alt="Photo from HackRPI X 2023"
+							src="/lastYearPhotos/IMG_1115.jpg"
+							alt="HackRPI X 2023 Photo"
+							width={1000}
+							height={1000}
 						/>
 					</div>
-					<div className="inline-grid">
+					<div className="col-span-4 grid grid-cols-2 gap-5">
+						<div className="col-span-1">
+							<Image
+								src="/lastYearPhotos/IMG_1118.jpg"
+								alt="HackRPI X 2023 Photo"
+								width={500}
+								height={500}
+							/>
+						</div>
+						<div className="col-span-1">
+							<Image
+								src="/lastYearPhotos/IMG_1123.jpg"
+								alt="HackRPI X 2023 Photo"
+								width={500}
+								height={500}
+							/>
+						</div>
+						<div className="col-span-1">
+							<Image
+								src="/lastYearPhotos/IMG_1126.jpg"
+								alt="HackRPI X 2023 Photo"
+								width={500}
+								height={500}
+							/>
+						</div>
+						<div className="col-span-1">
+							<Image
+								src="/lastYearPhotos/IMG_1127.jpg"
+								alt="HackRPI X 2023 Photo"
+								width={500}
+								height={500}
+							/>
+						</div>
+					</div>
+					<div className="col-span-2 grid grid-rows-2 gap-5">
+						<div className="col-span-1">
+							<Image
+								src="/lastYearPhotos/IMG_1131.jpg"
+								alt="HackRPI X 2023 Photo"
+								width={500}
+								height={500}
+							/>
+						</div>
+						<div className="col-span-1">
+							<Image
+								src="/lastYearPhotos/IMG_1135.jpg"
+								alt="HackRPI X 2023 Photo"
+								width={500}
+								height={500}
+							/>
+						</div>
+					</div>
+					<div className="col-span-4">
 						<Image
-							src={image2}
-							width={500}
-							height={500}
-							alt="Photo from HackRPI X 2023"
+							src="/lastYearPhotos/IMG_1144.jpg"
+							alt="HackRPI X 2023 Photo"
+							width={1000}
+							height={1000}
 						/>
 					</div>
-					<div className="inline-grid">
-						<Image
-							src={image3}
-							width={500}
-							height={500}
-							alt="Photo from HackRPI X 2023"
-						/>
+					<div className="col-span-2 grid grid-rows-2 gap-5">
+						<div className="col-span-1">
+							<Image
+								src="/lastYearPhotos/IMG_1148.jpg"
+								alt="HackRPI X 2023 Photo"
+								width={500}
+								height={500}
+							/>
+						</div>
+						<div className="col-span-1">
+							<Image
+								src="/lastYearPhotos/IMG_1151.jpg"
+								alt="HackRPI X 2023 Photo"
+								width={500}
+								height={500}
+							/>
+						</div>
 					</div>
-					<div className="inline-grid">
-						<Image
-							src={image4}
-							width={500}
-							height={500}
-							alt="Photo from HackRPI X 2023"
-						/>
+					<div className="col-span-4 grid grid-cols-2 gap-5">
+						<div className="col-span-1">
+							<Image
+								src="/lastYearPhotos/IMG_1153.jpg"
+								alt="HackRPI X 2023 Photo"
+								width={500}
+								height={500}
+							/>
+						</div>
+						<div className="col-span-1">
+							<Image
+								src="/lastYearPhotos/IMG_1157.jpg"
+								alt="HackRPI X 2023 Photo"
+								width={500}
+								height={500}
+							/>
+						</div>
+						<div className="col-span-1">
+							<Image
+								src="/lastYearPhotos/IMG_1162.jpg"
+								alt="HackRPI X 2023 Photo"
+								width={500}
+								height={500}
+							/>
+						</div>
+						<div className="col-span-1">
+							<Image
+								src="/lastYearPhotos/IMG_1169.jpg"
+								alt="HackRPI X 2023 Photo"
+								width={500}
+								height={500}
+							/>
+						</div>
 					</div>
-					<div className="inline-grid">
+					<div className="col-span-4">
 						<Image
-							src={image5}
-							width={500}
-							height={500}
-							alt="Photo from HackRPI X 2023"
+							src="/lastYearPhotos/IMG_1179.jpg"
+							alt="HackRPI X 2023 Photo"
+							width={1000}
+							height={1000}
 						/>
-					</div>
-					<div className="inline-grid">
-						<Image
-							src={image6}
-							width={500}
-							height={500}
-							alt="Photo from HackRPI X 2023"
-						/>
-
-					</div>
-					<div className="inline-grid">
-						<Image
-							src={image7}
-							width={500}
-							height={500}
-							alt="Photo from HackRPI X 2023"
-						/>
-                        
-					</div>
-					<div className="inline-grid">
-						<Image
-							src={image8}
-							width={500}
-							height={500}
-							alt="Photo from HackRPI X 2023"
-						/>
-
-					</div>
-					<div className="inline-grid">
-						<Image
-							src={image9}
-							width={500}
-							height={500}
-							alt="Photo from HackRPI X 2023"
-						/>
-
 					</div>
 				</div>
 			</div>
