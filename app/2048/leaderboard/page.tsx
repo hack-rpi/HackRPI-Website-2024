f21c9e9d708fe586b6ae776baff2275e8e1d0f2c
CMarink <christianm7765@gmail.com>
2024-10-26 17:55:48 -0400

    Finished Leaderboard Frontend
    Created HTML table for the leaderboard which will take in table data from getLeaderboard()

--- app/2048/leaderboard/page.tsx
+++ app/2048/leaderboard/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+import { useEffect } from "react";
 import NavBar from "@/components/nav-bar/nav-bar";
 import Footer from "@/components/footer/footer";
 import { Amplify } from "aws-amplify";
@@ -22,15 +24,48 @@ type LeaderboardEntry = {
 }
 
 export default function (){
+    const [leaderboardEntries, setLeaderboardEntries] = useState<LeaderboardEntry[]>([]);
+
+    useEffect(() => {
+        const fetchLeaderboard = async () => {
+            const entries = await getLeaderboard();
+            setLeaderboardEntries(entries);
+        };
+        
+        fetchLeaderboard();
+    }, []);
+
     return (
         <div className="flex flex-col items-start desktop:items-center justify-start w-full h-full">
             <NavBar showOnScroll={false}></NavBar>
 
+            <div className="flex-grow mt-30"></div>
             <div className="flex-grow flex-shrink basis-auto">
-                
-            </div>
+                <h1 className="mt-28 text-center text-4xl">2048 Leaderboard</h1>
 
-            <div className="absolute bottom-0 w-full">
+                <table className="min-w-[80vw] mt-10 justify-inbetween min-w-full table-auto w-full">
+                    <thead>
+                        <tr>
+                            <th className="w-1/3 px-4 py-2 border">Username</th>
+                            <th className="w-1/3 px-4 py-2 border">Score</th>
+                            <th className="w-1/3 px-4 py-2 border">Year</th>
+                        </tr>
+                    </thead>
+
+                    <tbody className="text-center">
+                        {leaderboardEntries.map((entry) => (
+                            <tr key={entry.id}>
+                                <td className="px-4 py-2 border">{entry.username}</td>
+                                <td className="px-4 py-2 border">{entry.score}</td>
+                                <td className="px-4 py-2 border">{entry.year}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+            <div className="flex-grow mt-24"></div>
+
+            <div className="absolute-bottom-0 w-full">
                 <Footer />
             </div>
         </div> 
@@ -59,6 +94,6 @@ async function getLeaderboard(): Promise<LeaderboardEntry[]> {
         console.error(errors);
         return [];
     }
-    
+
     return data.map((entry)=> entry as LeaderboardEntry);
 }
