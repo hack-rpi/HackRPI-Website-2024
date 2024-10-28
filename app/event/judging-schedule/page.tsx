f3a3b3c2203cf94448397a260919c3b9a0243db6
Cooper Werner <cooperwerner824@gmail.com>
2024-10-28 19:54:13 -0400

    update judging schedule text

--- app/event/judging-schedule/page.tsx
+++ app/event/judging-schedule/page.tsx
@@ -13,7 +13,7 @@ export default function Page() {
 			<div className="flex flex-col w-full flex-grow flex-shrink basis-auto mt-24 lg:mt-16 items-center">
 				<h1 className="text-4xl sm:text-6xl font-bold font-sans my-4 text-center">Judging Schedule</h1>
                 <p className="text-lg sm:text-xl font-sans my-4 text-center w-11/12">Please make sure you are at your assigned room at least 5 minutes before your assigned time! Thank you!</p>
-                <p className="text-lg sm:text-xl font-sans my-4 text-center">The judging schedule will be released soon. Please check back later.</p>
+                <p className="text-lg sm:text-xl font-sans my-4 text-center">The judging schedule will be released around 10:45 am on November 10th.</p>
                 {/* <div className="bg-pink-500 h-fit w-2/3">
                     <div className="h-[500px]"></div>
                 </div> */}
