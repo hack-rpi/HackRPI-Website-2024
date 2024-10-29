ec2447c67be56040d9334c69a7a12f8370292656
anthonyrys <anthonyrys05@gmail.com>
2024-10-28 20:56:12 -0400

    Add resource slides

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import GoogleSlides from "react-google-slides";
+
 import NavBar from "@/components/nav-bar/nav-bar";
 import "@/app/globals.css";
 import Footer from "@/components/footer/footer";
@@ -10,26 +12,66 @@ export default function page() {
 	return (
 		<div className="flex flex-col w-full h-fit min-h-screen items-center justify-center">
 			<NavBar showOnScroll={false} />
-			<div className="flex flex-col w-11/12 md:w-1/2 flex-grow flex-shrink basis-auto items-center justify-center text-center desktop:text-left">
-				<div className="h-24 desktop:h-16"></div>
-				<Image src={logo} alt="HackRPI Logo" width={100} height={100} />
-				<h1 className="text-4xl font-bold mb-4">🚧 HackRPI Site Under Development! 🚧</h1>
-				<p className="text-xl mb-2">
-					Attention all code warriors and caffeine aficionados! Our hackathon hub is currently in the middle of an epic
-					upgrade. We&apos;re coding faster than a team of over-caffeinated developers at 3 AM.
-				</p>
-				<ul className="list-disc w-full list-inside mb-2">
-					<li>Challenges so brain-bending, they&apos;ll make your algorithms sweat!</li>
-					<li>Prizes so cool, even your keyboard will do a happy dance.</li>
-					<li>Resources so valuable, they&apos;ll make Stack Overflow blush.</li>
-				</ul>
-				<p className="mb-2">
-					Stay tuned for the ultimate hackathon experience. In the meantime, feel free to practice your speed coding and
-					stock up on energy drinks. This is going to be one heck of a digital showdown.
-				</p>
-				<p className="w-full mb-2">
-					Thanks for your patience! (And for not accidentally deploying anything to production.)
-				</p>
+			<div className="flex flex-wrap justify-center gap-12 m-10 pt-10 pb-10">
+				<div className="flex flex-col justify-center items-center">
+					<h2 id="web-dev" className="text-3xl font-bold text-center mb-8 pt-10">Web Development</h2>
+					<GoogleSlides
+						width={640}
+						height={480}
+						slidesLink="https://docs.google.com/presentation/d/1ev4z8gbvVjq_Jh8wBae-ulIzclSElyzKslLN304oeHA/edit?usp=sharing"
+						position={1}
+						showControls
+						loop
+					/>
+				</div>
+
+				<div className="flex flex-col justify-center items-center">
+					<h2 id="mobile-dev" className="text-3xl font-bold text-center mb-8 pt-10">Mobile Development</h2>
+					<GoogleSlides
+						width={640}
+						height={480}
+						slidesLink="https://docs.google.com/presentation/d/1pE-DWfhqWjCKAdXuzGSdiv_oHa7MTDD05ZS3qsWBRKU/edit?usp=sharing"
+						position={1}
+						showControls
+						loop
+					/>
+				</div>
+
+				<div className="flex flex-col justify-center items-center">
+					<h2 id="game-dev" className="text-3xl font-bold text-center mb-8 pt-10">Game Development</h2>
+					<GoogleSlides
+						width={640}
+						height={480}
+						slidesLink="https://docs.google.com/presentation/d/1WhbCJRoPq63Y-YguBSMBEcSZnTYtZ8D98zsf5h2hCdg/edit?usp=sharing"
+						position={1}
+						showControls
+						loop
+					/>
+				</div>
+
+				<div className="flex flex-col justify-center items-center">
+					<h2 id="cloud" className="text-3xl font-bold text-center mb-8 pt-10">Cloud Development</h2>
+					<GoogleSlides
+						width={640}
+						height={480}
+						slidesLink="https://docs.google.com/presentation/d/1-HVKKPet-yn6Ahh7uAX-xqg2Tn6BmCiu0uOIdznt_c8/edit?usp=sharing"
+						position={1}
+						showControls
+						loop
+					/>
+				</div>
+	
+				<div className="flex flex-col justify-center items-center">
+					<h2 id="submissions" className="text-3xl font-bold text-center mb-8 pt-10">Submitting Your Project</h2>
+					<GoogleSlides
+						width={640}
+						height={480}
+						slidesLink="https://docs.google.com/presentation/d/1-xy1srb7m4WPzRnYeKQvtGsBijbHcewqIhexL6Yn2SQ/edit?usp=sharing"
+						position={1}
+						showControls
+						loop
+					/>
+				</div>
 			</div>
 			<Footer />
 		</div>
