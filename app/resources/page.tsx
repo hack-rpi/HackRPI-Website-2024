48a4f32e1785db627cbb0a22628b6a9005244f29
Cooper Werner <cooperwerner824@gmail.com>
2024-11-05 10:43:19 -0500

    add sponsor resources

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -3,6 +3,7 @@
 import "@/app/globals.css";
 import NavBar from "@/components/nav-bar/nav-bar";
 import Footer from "@/components/footer/footer";
+import HackRPILink from "@/components/themed-components/hackrpi-link";
 
 export default function page() {
 	return (
@@ -14,7 +15,7 @@ export default function page() {
 						Web Development
 					</h2>
 					<iframe
-						src="https://docs.google.com/presentation/d/e/2PACX-1vShAEu5LxeaY_QFG5W509bUy0wCJl2nFq5D8YNqzm0EHBHtcF0Oq_RBndqe4CeaSCl3nSFQ4JVQeImR/embed?start=false&loop=false&delayms=3000"
+						src="https://docs.google.com/presentation/d/e/2PACX-1vQ1Y1jocoVjEzR9OR0fVVsbQrc05vaqpPXpIPhxRvHu6G0u0fSw7bWmijLKVmx8VyCrkbY2rZE_VEjr/embed?start=false&loop=false&delayms=3000"
 						className="w-full max-w-[480px] aspect-video"
 						allowFullScreen={true}
 					></iframe>
@@ -62,6 +63,43 @@ export default function page() {
 						className="w-full max-w-[480px] aspect-video"
 						allowFullScreen={true}
 					></iframe>
+				</div>
+
+				<div className="flex flex-col justify-center items-center w-80 xs:w-[400px] md:w-[600px]">
+					<h2 id="claude" className="text-3xl font-bold text-center mb-8 pt-10">
+						Anthropic Claude's Computer Use API
+					</h2>
+					<p>
+						The upgraded Claude 3.5 Sonnet model is capable of interacting with tools that can manipulate a computer
+						desktop environment. See the documentation at the link below for more information on how to use the API.
+					</p>
+					<HackRPILink
+						href="https://docs.anthropic.com/en/docs/build-with-claude/computer-use"
+						className="px-4 py-4 mt-4"
+					>
+						Anthropic Claude's Computer Use API
+					</HackRPILink>
+				</div>
+
+				<div className="flex flex-col justify-center items-center w-80 xs:w-[400px] md:w-[600px]">
+					<h2 id="wolfram" className="text-3xl font-bold text-center mb-8 pt-10">
+						Wolfram|One API
+					</h2>
+					<p>
+						Wolfram|One is a powerful computational software that can be used for a variety of applications. Check out
+						the documentation at the link below to learn more about the API and how to use it in your projects.
+					</p>
+					<HackRPILink href="https://account.wolfram.com/redeem/zHackRPI1124" className="px-4 py-4 my-4">
+						Wolfram|One API
+					</HackRPILink>
+					<p>
+						This URL will take you to a sign-in page and prompt you to log in using your Wolfram ID. If you don't
+						already have a Wolfram ID, you'll be prompted to create one using an active email address. Once logged in,
+						you will see in the Downloads section which platforms of the software you have access to, with their
+						accompanying activation keys. Please download the desktop version to your machine, boot up the application
+						and use the associated activation key when prompted. This will grant you full access for 30 days from your
+						activation date.
+					</p>
 				</div>
 			</div>
 			<Footer />
