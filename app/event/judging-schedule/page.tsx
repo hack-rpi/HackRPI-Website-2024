f00dd6059bbfc6bc855f29b80721e135773cfa83
Cooper Werner <cooperwerner824@gmail.com>
2024-11-10 11:19:07 -0500

    add schedule image

--- app/event/judging-schedule/page.tsx
+++ app/event/judging-schedule/page.tsx
@@ -2,7 +2,7 @@
 
 import Footer from "@/components/footer/footer";
 import NavBar from "@/components/nav-bar/nav-bar";
-// import Image from "next/image";
+import Image from "next/image";
 import "@/app/globals.css";
 
 export default function Page() {
@@ -14,21 +14,15 @@ export default function Page() {
 				<p className="text-lg sm:text-xl font-sans my-4 text-center w-11/12">
 					Please make sure you are at your assigned room at least 5 minutes before your assigned time! Thank you!
 				</p>
-				<p className="text-lg sm:text-xl font-sans my-4 text-center">
+				{/* <p className="text-lg sm:text-xl font-sans my-4 text-center">
 					The judging schedule will be released around 10:45 am on November 10th.
-				</p>
+				</p> */}
 				{/* <div className="bg-pink-500 h-fit w-2/3">
                     <div className="h-[500px]"></div>
                 </div> */}
-				{/* <div className="w-11/12 desktop:w-2/3 h-fit">
-					<Image
-						src="/images/judging-schedule.png"
-						alt="Judging Schedule"
-						width={1200}
-						height={1200}
-						className="rounded-lg"
-					/>
-				</div> */}
+				<div className="w-11/12 desktop:w-2/3 h-fit">
+					<Image src="/Judging-Schedule.png" alt="Judging Schedule" width={1200} height={1200} className="rounded-lg" />
+				</div>
 			</div>
 			<Footer />
 		</div>
