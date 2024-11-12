0c7c3e3ce0a02f60a58efe99e6356402aa9e997b
Cooper Werner <cooperwerner824@gmail.com>
2024-11-11 19:29:58 -0500

    update registration link to point to winners

--- components/themed-components/registration-link.tsx
+++ components/themed-components/registration-link.tsx
@@ -2,8 +2,8 @@ import HackRPILink from "./hackrpi-link";
 
 export default function RegistrationLink({ className }: { className?: string }) {
 	return (
-		<HackRPILink href="https://events.mlh.io/events/11258-hackrpi" className={`${className} pl-2 pr-6 py-2`}>
-			Register Now
+		<HackRPILink href="https://hackrpi2024.devpost.com/project-gallery" className={`${className} pl-2 pr-6 py-2`}>
+			See Winners
 		</HackRPILink>
 	);
 }
